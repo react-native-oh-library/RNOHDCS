72e44b90478842fa68a31e600b654f4ab1ea6a90
Allison_zhou <zhouyongchun1@h-partners.com>
2024-03-14 14:33:58 +0800

    update markdown demo

--- MarkdownDemo/Style.tsx
+++ MarkdownDemo/Style.tsx
@@ -34,7 +34,7 @@ const App: () => React$Node = () => {
               style={{
                 body: {color: 'red', fontSize: 10},
                 heading1: {color: 'purple'},
-                fence: {color: 'black', fontSize: 14}
+                code_block: {color: 'black', fontSize: 14}
               }}
             >
               {copy}
