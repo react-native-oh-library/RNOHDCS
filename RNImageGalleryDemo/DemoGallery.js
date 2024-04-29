07042a1171a76af208b14ab9fe8ec57d031c6c5c
qin <1851513221@qq.com>
2024-04-29 20:41:15 +0800

    提交TestCase文件

--- RNImageGalleryDemo/DemoGallery.js
+++ RNImageGalleryDemo/DemoGallery.js
@@ -2,7 +2,7 @@ import React, { Component, useState } from 'react';
 import { View, Text, Dimensions, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import Gallery from 'react-native-image-gallery';
 import { Tester, TestSuite } from '@rnoh/testerino';
-import { TestCase } from '../../components';
+import {TestCase} from './testCase';
 const { height, width } = Dimensions.get('window');
 const flatListProps = {
     // 初始渲染的item数量  
