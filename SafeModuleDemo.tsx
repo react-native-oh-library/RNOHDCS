c83b0746549816117e1018a7e4bd1b27fdc77066
xiawenqiang <xiawenqiang4@h-partners.com>
2024-05-13 17:34:36 +0800

    Add safe module demo.

---
+++ SafeModuleDemo.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Button } from 'react-native';
+import SafeModule from '@react-native-oh-tpl/react-native-safe-module';
+
+const App = () => {
+  const NativeLottieView = SafeModule.component({
+    viewName: 'LottieAnimationView',
+    mockComponent: View,
+  });
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      
+      <NativeLottieView
+        style={{backgroundColor: 'red', width: 100, height: 100}}
+        source={undefined}
+        sourceName={undefined}
+        sourceJson={undefined}
+      />
+    </View>
+  );
+};
+
+export default App;
