88e19595cd1e735ec4f219f8fc93da01140c7575
逸先生 <qinlianao@h-partners.com>
2024-05-31 17:32:47 +0800

    修改demo格式

--- react-native-ratings/RatingsDemo.tsx
+++ react-native-ratings/RatingsDemo.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Tester, TestSuite } from '@rnoh/testerino';
 import { View, Text, StyleSheet } from 'react-native';
 import { Rating, AirbnbRating } from 'react-native-ratings';
 
@@ -18,7 +19,7 @@ const styles = StyleSheet.create({
   }
 });
 
-export function RatingsDemo () {
+export function RatingsDemo() {
   const [rating, setRating] = useState(3);
 
   const ratingCompleted = (rating: number) => {
@@ -27,54 +28,56 @@ export function RatingsDemo () {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Standard Rating</Text>
-      <Rating
-        type="star"
-        ratingCount={5}
-        imageSize={30}
-        showRating
-        onFinishRating={ratingCompleted}
-      />
+      <TestSuite name="Ratinging">
+        <View style={styles.container}>
+          <Text style={styles.title}>Standard Rating</Text>
+          <Rating
+            type="star"
+            ratingCount={5}
+            imageSize={30}
+            showRating
+            onFinishRating={ratingCompleted}
+          />
 
-      <Text style={styles.title}>Custom Icon Rating</Text>
-      <Rating
-        type="custom"
-        ratingImage={require('../assets/expo.png')}
-        ratingColor="#3498db"
-        ratingBackgroundColor="#c8c7c8"
-        ratingCount={6}
-        imageSize={40}
-        onFinishRating={ratingCompleted}
-        style={{ paddingVertical: 10 }}
-      />
+          <Text style={styles.title}>Custom Icon Rating</Text>
+          <Rating
+            type="custom"
+            ratingImage={require('../assets/expo.png')}
+            ratingColor="#3498db"
+            ratingBackgroundColor="#c8c7c8"
+            ratingCount={6}
+            imageSize={40}
+            onFinishRating={ratingCompleted}
+            style={{ paddingVertical: 10 }}
+          />
 
-      <Text style={styles.title}>Airbnb Rating</Text>
-      <AirbnbRating
-        count={5}
-        reviews={["Terrible", "Bad", "Okay", "Good", "Great"]}
-        defaultRating={3}
-        size={20}
-        onFinishRating={ratingCompleted}
-      />
+          <Text style={styles.title}>Airbnb Rating</Text>
+          <AirbnbRating
+            count={5}
+            reviews={["Terrible", "Bad", "Okay", "Good", "Great"]}
+            defaultRating={3}
+            size={20}
+            onFinishRating={ratingCompleted}
+          />
 
-      <Text style={styles.title}>Read-only Rating</Text>
-      <Rating
-        readonly
-        startingValue={rating}
-        ratingCount={5}
-        imageSize={30}
-      />
+          <Text style={styles.title}>Read-only Rating</Text>
+          <Rating
+            readonly
+            startingValue={rating}
+            ratingCount={5}
+            imageSize={30}
+          />
 
-      <Text style={styles.title}>Fractional Rating</Text>
-      <Rating
-        fractions={2}
-        startingValue={2.5}
-        ratingCount={5}
-        imageSize={30}
-        onFinishRating={ratingCompleted}
-      />
-    </View>
+          <Text style={styles.title}>Fractional Rating</Text>
+          <Rating
+            fractions={2}
+            startingValue={2.5}
+            ratingCount={5}
+            imageSize={30}
+            onFinishRating={ratingCompleted}
+          />
+        </View>
+      </TestSuite>
   );
 };
 
