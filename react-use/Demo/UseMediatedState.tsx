f8e8b8f2660a69390022f5b91e85b193d6b462eb
张浩 <zhanghao591@h-partners.com>
2024-05-31 19:10:53 +0800

    修改demo中的错误

--- react-use/Demo/UseMediatedState.tsx
+++ react-use/Demo/UseMediatedState.tsx
@@ -11,7 +11,7 @@ const inputMediator = s => s.replace(/[\s]+/g, ' ');
       <Text>You will not be able to enter more than one space</Text>
       <Text>您将无法输入多个空格</Text>
       <TextInput
-        style={{ borderWidth: 1 }}
+        style={{ borderWidth: 1,fontSize: 16 }}
         value={state}
         onChangeText={(value) => {
           setState(value);
