fbe3154fd0f3dc1463b04bf155945900d250846e
Magic-Neko <312651922@qq.com>
2024-06-03 18:40:25 +0800

    modal更新

--- react-native-modal/ModalDemo.tsx
+++ react-native-modal/ModalDemo.tsx
@@ -5,7 +5,7 @@ import Modal from "react-native-modal";
 import {TestSuite,TestCase,Tester} from '@rnoh/testerino';
  
 
-export function ModalDemo() {
+export function ModalDemo(this: any) {
 
     const [isModalVisible, setModalVisible] = useState(false);
 
@@ -17,14 +17,33 @@ export function ModalDemo() {
   return (
       
     <Tester>
-      <TestSuite name='MaterialTextfielText'>
-        <TestCase itShould='Default Material Textfiel Text'>
+      <TestSuite name='react-native-modal'>
+        <TestCase itShould='Default react-native-modal'>
         <Button label='Show modal' onPress={toggleModal} />
 
-       <Modal isVisible={isModalVisible} animationIn="fadeInLeft">
+       <Modal isVisible={isModalVisible} animationIn="slideInUp"
+        onBackButtonPress={() => { 
+          console.warn("onBackButtonPress"); 
+        }} 
+        onModalShow={() => { 
+          console.warn("onModalShow"); 
+        }}
+        onSwipeStart={() => { 
+          console.warn("onSwipeStart"); 
+        }} 
+        onModalWillHide={() => { 
+          console.warn("onModalWillHide"); 
+        }} 
+        onModalHide={() => { 
+          console.warn("onModalHide"); 
+        }} 
+        onModalWillShow={() => { 
+          console.warn("onModalWillShow"); 
+        }} 
+        animationInTiming={400} animationOut={'slideOutDown'} backdropColor= 'black' >
        <View style={{ flex: 1 }}>
        <Text>Hello!</Text>
-
+       
     <Button label='Hide modal' onPress={toggleModal} />
     </View>
     </Modal>
