4edf246a21661f402fa11fdff8c6f46c34e5b603
hewei <hewei203@h-partners.com>
2024-06-20 18:23:46 +0800

    fix: 修改react-native-restart测试demo

--- react-native-restart/RestartDemo.tsx
+++ react-native-restart/RestartDemo.tsx
@@ -1,19 +1,17 @@
 import {Button, Text, View, Alert} from 'react-native';
 import RNRestart from 'react-native-restart';
-import {Tester, TestSuite} from '@rnoh/testerino';
-import {TestCase} from '../../components/TestCase';
+import {Tester, TestSuite, TestCase} from '@rnoh/testerino';
 
-
-export default function RestartDemoTester() {
+export function RestartDemoTester() {
   return (
     <Tester>
       <TestSuite name="restart">
-        <TestCase.Example tags={['C_API']} itShould="restart">
+        <TestCase tags={['C_API']} itShould="restart">
           <Button title="重启(restart)" onPress={() => RNRestart.restart()} />
-        </TestCase.Example>
-        <TestCase.Example tags={['C_API']} itShould="Restart">
+        </TestCase>
+        <TestCase tags={['C_API']} itShould="Restart">
           <Button title="重启(Restart)" onPress={() => RNRestart.Restart()} />
-        </TestCase.Example>
+        </TestCase>
       </TestSuite>
     </Tester>
   );
