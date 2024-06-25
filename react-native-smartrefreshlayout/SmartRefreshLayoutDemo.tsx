2690a5c092011b76b6f8567a438a68dfd896bbf0
tyBrave <tianyong21@h-partners.com>
2024-06-25 19:50:34 +0800

    refactor: smartRefreshLayout demo is adapted to c again
    Signed-off-by: tyBrave <tianyong21@h-partners.com>

--- react-native-smartrefreshlayout/SmartRefreshLayoutDemo.tsx
+++ react-native-smartrefreshlayout/SmartRefreshLayoutDemo.tsx
@@ -1,31 +1,24 @@
-import React, { useState } from 'react';
-import { View, Text, FlatList, StyleSheet ,TouchableOpacity} from 'react-native';
-import { SmartRefreshControl,AnyHeader} from 'react-native-smartrefreshlayout';
+import React, { useState, useRef } from 'react';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Image, RefreshControl, Animated, TextInput } from 'react-native';
+
+import { SmartRefreshControl, AnyHeader, DefaultHeader, ClassicsHeader, MeaterialHeader } from 'react-native-smartrefreshlayout';
 
 const App = () => {
-  const [text, setText] = useState("状态");
-  const [text1, setText1] = useState("刷新时间");
+  const [text, setText] = useState('刷新时间');
+  const [headerHeight, setHeaderHeight] = useState(69);
+  const [anyheaderHeight, setAnyHeaderHeight] = useState(0);
 
-  const [headerHeight, setHeaderHeight] = useState(66);
-  const [color, setColor] = useState('#fff000');
-
-
-  const [data, setData] = useState([
-    { id: 1, text: 'Item 1' },
-    { id: 2, text: 'Item 2' },
-    { id: 3, text: 'Item 2' },
-
-    { id: 4, text: 'Item 2' },
-    { id: 5, text: 'Item 2' },
-    { id: 6, text: 'Item 2' },
-    { id: 7, text: 'Item 7' },
-    { id: 8, text: 'Item 7' },
-    { id: 9, text: 'Item 7' },
-    { id: 10, text: 'Item 7' },
-    { id: 11, text: 'Item 11' },
-
-    // ... more data ...
-  ]);
+  const [imgHeight, setImgHeight] = useState(0.0);
+  const [color, setColor] = useState('#ffff00');
+  const [color1, setColor1] = useState('#ffff00');
+  const generateArray = (size: number) => {
+    const arr = new Array(size);
+    for (let i = 0; i < size; i++) {
+      arr[i] = { id: i, text: `Item ${i}` }
+    }
+    return arr;
+  };
+  const [data, setData] = useState(generateArray(100));
 
   const onLoadMore = () => {
     // Simulate loading more data
@@ -38,70 +31,82 @@ const App = () => {
     }, 1000);
   };
 
-  const renderItem = ({ item }) => (
+  const ItemView = ({ item }) => (
     <View style={styles.item}>
-      <Text style={styles.item}>{item.text}</Text>
+      <Text style={styles.itemText} onPress={() => {
+        console.log("smartLayout itemText:" + item.text)
+      }}>{item.text}</Text>
     </View>
   );
-  let smartRefreshControlRef:React.RefObject<SmartRefreshControl>;
+  let smartRefreshControlRef: React.RefObject<SmartRefreshControl> = useRef(null);
   return (
     <View >
-      <TouchableOpacity onPress={() => {smartRefreshControlRef.finishRefresh({delayed:-1,success:true})}}>
-      <Text style={{height:40,width:'100%',backgroundColor:"red"}} >finish</Text>
+      <SmartRefreshControl
+        ref={smartRefreshControlRef}
+        primaryColor={color}
+        autoRefresh={{ 'refresh': false, 'time': 5000 }}
+        headerHeight={headerHeight}
+        style={{ height: '100%', width: '100%', backgroundColor: '#ffffff' }}
+        onReleaseToRefresh={e => {
+          console.log("smartLayout RN onReleaseToRefresh:" + JSON.stringify(e.nativeEvent))
+        }}
+        onRefresh={() => {
+          setText('时间：' + new Date().getTime() + 'onRefresh触发刷新');
+          setTimeout(() => {
+            smartRefreshControlRef.current.finishRefresh({ delayed: -1, success: true });
+          }, 5000);
+          setText('时间：' + new Date().getTime() + 'onRefresh刷新完成');
+        }}
+        onHeaderReleasing={(data: any) => {
+          console.log("smartLayout RN onHeaderReleasing:" + JSON.stringify(data.nativeEvent))
+        }}
+        onHeaderPulling={(data: any) => {
+          console.log("smartLayout RN onHeaderPulling:" + JSON.stringify(data.nativeEvent))
+        }}
+        onPullDownToRefresh={() => {
+          console.log("smartLayout RN onPullDownToRefresh")
+        }}
+        onHeaderMoving={(data: any) => {
+          console.log("smartLayout RN onHeaderMoving:" + data.nativeEvent.offset)
+          setAnyHeaderHeight(data.nativeEvent.offset)
+          if (data.nativeEvent.offset > 69) {
+            return;
+          }
+          setImgHeight(data.nativeEvent.offset);
+        }}
+        HeaderComponent={
+          // <MeaterialHeader/>
+		  // <ClassicsHeader/>
+          // <DefaultHeader
+          // primaryColor={'#ffff00'}/>
 
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => {setHeaderHeight(headerHeight == 66?132:66)}}>
-          <Text style={{height:40,width:'100%',backgroundColor:"pink"}} >切换高度 66/132</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => {setColor(color === "#fff000"?"red":"#fff000")}}>
-          <Text style={{height:40,width:'100%',backgroundColor:"green"}} >切换颜色（正在刷新中不支持切换背景色）</Text>
-        </TouchableOpacity>
-
-      <Text style={{height:40,width:'100%',backgroundColor:""}}>
-        {text}
-        </Text>
-        <Text style={{height:40,width:'100%',backgroundColor:""}}>
-        {text1}
-        </Text>
-    <SmartRefreshControl
-    ref={(ref)=>smartRefreshControlRef=ref}
-      primaryColor={color}
-      headerHeight={headerHeight}
-      style={{ height:500,width:'100%',backgroundColor:"#ffcc00"}}
-      onReleaseToRefresh={(e)=>{
-        setText('onPullDownToRefresh' + JSON.stringify(e.nativeEvent))
-      }}
-      onRefresh={
-        ()=>{
-          setText1('时间：' + new Date().getTime() + "onRefresh触发刷新")
-        }
-      }
-      HeaderComponent={
-        <AnyHeader ><Text style={{height:66,width:'100%'}}>{text}</Text></AnyHeader>
-      }
-
-    >
-      <FlatList
-      style={{ flex: 1 ,height:'100%',width:'100%'}}
-
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={item => item.id.toString()}
-      />
-    </SmartRefreshControl>
+          <AnyHeader style={{ width: '100%', height: 0 }}>
+            <Animated.View style={{ height: 69,  width: '100%',justifyContent: "center", alignItems: 'center', backgroundColor: "red" }}>
+              <Image style={{width:imgHeight,height:imgHeight}} source={require('./img/load.gif')} />
+              {/* <Text style={{ height: 50, width: '100%', textAlign: 'center', textAlignVertical: 'center', color: '#000000', backgroundColor: "green" }}>{'请求头部'}</Text> */}
+            </Animated.View>
+          </AnyHeader>
+        }>
+        <FlatList
+          style={{ flex: 1, height: '100%', width: '100%', backgroundColor: '#ffffff' }}
+          data={data}
+          renderItem={({ item }) => <ItemView item={item} />}
+          keyExtractor={item => item.id.toString()}
+        />
+      </SmartRefreshControl>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   item: {
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-    width:100,
-    height:100,
+    backgroundColor: '#f9f9f9',
+    padding: 10,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  itemText: {
+    fontSize: 16,
   },
 });
 
