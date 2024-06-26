4ef8651de310234c9478555d839a19fb0273f74d
zhyx2 <zhuyongxiang2@h-partners.com>
2024-06-26 10:22:40 +0800

    test: 增加flash-list三方库demo
    Signed-off-by: zhyx2 <zhuyongxiang2@h-partners.com>

---
+++ @shopify/flash-list/capi/FlashListDemo/NavigationTree.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import CustomDemoList from "./CustomDemoList"
+import List from "./List";
+import PaginatedList from "./PaginatedList";
+import { } from "./Messages";
+import { Twitter, TweetDetailScreen } from "./twitter";
+import ContactsSectionList from "./contacts/ContactsSectionList";
+import Contacts from "./contacts/Contacts";
+import { RootStackParamList } from "./constants";
+import { ExamplesScreen } from "./ExamplesScreen";
+import { DebugScreen } from "./Debug";
+import { Messages } from "./Messages";
+// import TwitterBenchmark from "./twitter/TwitterBenchmark";
+// import TwitterCustomCellContainer from "./twitter/CustomCellRendererComponent";
+import { Masonry } from "./Masonry";
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const NavigationTree = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Group>
+          <Stack.Screen name="Examples" component={ExamplesScreen} />
+          <Stack.Screen name="List" component={List} />
+          <Stack.Screen name="PaginatedList" component={PaginatedList} />
+          <Stack.Screen name="Twitter" component={Twitter} />
+          <Stack.Screen name="CustomDemoList" component={CustomDemoList} />
+          <Stack.Screen name="Contacts" component={Contacts} />
+          <Stack.Screen
+            name="ContactsSectionList"
+            component={ContactsSectionList}
+            options={{ title: "Contacts" }}
+          />
+          <Stack.Screen
+            name="TweetDetailScreen"
+            component={TweetDetailScreen}
+          />
+          <Stack.Screen name="Messages" component={Messages} />
+        </Stack.Group>
+        <Stack.Screen name="Masonry" component={Masonry} />
+        <Stack.Group screenOptions={{ presentation: "modal" }}>
+          <Stack.Screen name="Debug" component={DebugScreen} />
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default NavigationTree;
