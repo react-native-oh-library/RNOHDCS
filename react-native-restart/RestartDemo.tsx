8b2e7629804333240fb9a4f0b1e1f131181a2fe4
hewei <hewei203@h-partners.com>
2024-06-28 18:38:48 +0800

    docs: 修改react-native-restart测试demo

--- react-native-restart/RestartDemo.tsx
+++ react-native-restart/RestartDemo.tsx
@@ -1,4 +1,4 @@
-import {Button, Text, View, Alert} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import RNRestart from 'react-native-restart';
 import {Tester, TestSuite, TestCase} from '@rnoh/testerino';
 
@@ -7,12 +7,70 @@ export function RestartDemoTester() {
     <Tester>
       <TestSuite name="restart">
         <TestCase tags={['C_API']} itShould="restart">
-          <Button title="重启(restart)" onPress={() => RNRestart.restart()} />
+          <Text
+            style={styles.button}
+            onPress={() => RNRestart.restart('restart_a')}>
+            restart
+          </Text>
         </TestCase>
         <TestCase tags={['C_API']} itShould="Restart">
-          <Button title="重启(Restart)" onPress={() => RNRestart.Restart()} />
+          <Text
+            style={styles.button}
+            onPress={() => RNRestart.Restart('restart_b')}>
+            Restart
+          </Text>
         </TestCase>
+        <TestCase
+          tags={['C_API']}
+          itShould="getReason"
+          initialState={'reason'}
+          arrange={({setState}) => {
+            return (
+              <Text
+                style={styles.button}
+                onPress={async () => {
+                  const reason = await RNRestart.getReason();
+                  setState(reason);
+                }}>
+                getReason
+              </Text>
+            );
+          }}
+          assert={async ({expect, state}) => {
+            expect(state).include('restart_');
+          }}
+        />
+        <TestCase
+          tags={['C_API']}
+          itShould="getName"
+          initialState={'getName'}
+          arrange={({setState}) => {
+            return (
+              <Text
+                style={styles.button}
+                onPress={() => {
+                  const nameStr = RNRestart.getName();
+                  setState(nameStr);
+                }}>
+                getName
+              </Text>
+            );
+          }}
+          assert={async ({expect, state}) => {
+            expect(state).equal('RNRestart');
+          }}
+        />
       </TestSuite>
     </Tester>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    backgroundColor: 'hsl(193, 95%, 68%)',
+    borderWidth: 2,
+    borderColor: 'hsl(193, 95%, 30%)',
+  },
+});
