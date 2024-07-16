bf55dc18a458918ac8785d819255d7299ee9602f
wangyizhou <95080306+JojobIId027@users.noreply.github.com>
2024-07-16 16:02:31 +0800

    fix: 修改 react-native-neomorph-shadows demo (#416)

--- react-native-neomorph-shadows/ShadowsDemo.tsx
+++ react-native-neomorph-shadows/ShadowsDemo.tsx
@@ -1,52 +1,161 @@
 import React from 'react';
-import { TestSuite } from '@rnoh/testerino';
-import { View, StyleSheet, Text } from 'react-native';
+import { TestSuite, Tester, TestCase } from '@rnoh/testerino';
+import { View, StyleSheet, Text, ScrollView } from 'react-native';
 import { Shadow } from 'react-native-neomorph-shadows';
 
-export function ShadowsDemo() {
+const ShadowsDemo = () => {
     return (
-        <TestSuite name="ShadowsDemo">
-            <View style={styles.container}>
-                <Shadow
-                    style={{
-                        fillOpacity: 0.6,
-                        stopColor: "#f0f0f0",//渐变结束的颜色
-                        startColor: "#FF3A3A",//渐变开始的颜色
-                        width: 110,
-                        height: 110,
-                        alignItems: "center",
-                        justifyContent: "center"
-                    }}>
-                    <View
-                        style={{
-                            borderRadius: 25,
-                            backgroundColor: "#FF3A3A",
-                            width: 110,
-                            height: 44,
-                            alignItems: "center",
-                            justifyContent: "center",
-                            // shadowColor: "#FFC0C0",
-                            // shadowOffset: {
-                            //     width: 0,
-                            //     height: 20
-                            // },
-                            // shadowOpacity: 1,
-                            // shadowRadius: 3.84,
-                            // elevation: 5 // 添加阴影效果
-                        }}>
-                        <Text
-                            style={{
-                                color: "#ffffff",
-                                fontWeight: "bold",
-                                textAlign: "center",
-                                fontSize: 16
-                            }}>
-                            登录/注册
-                        </Text>
-                    </View>
-                </Shadow>
-            </View>
-        </TestSuite>
+        <ScrollView>
+            <Tester style={{ flex: 1 }}>
+                <TestSuite name="ShadowsDemo">
+                    <TestCase
+                        tags={['C_API']}
+                        itShould="Tester Shadows Example">
+                        <View style={styles.container}>
+                            <Shadow
+                                style={{
+                                    fillOpacity: 0.6,
+                                    stopColor: "#f0f0f0",//渐变结束的颜色
+                                    startColor: "#FF3A3A",//渐变开始的颜色
+                                    width: 110,
+                                    height: 110,
+                                    alignItems: "center",
+                                    justifyContent: "center"
+                                }}>
+                                <View
+                                    style={{
+                                        borderRadius: 25,
+                                        backgroundColor: "#FF3A3A",
+                                        width: 110,
+                                        height: 44,
+                                        alignItems: "center",
+                                        justifyContent: "center",
+                                    }}>
+                                    <Text
+                                        style={{
+                                            color: "#ffffff",
+                                            fontWeight: "bold",
+                                            textAlign: "center",
+                                            fontSize: 16
+                                        }}>
+                                        登录/注册
+                                    </Text>
+                                </View>
+                            </Shadow>
+                        </View>
+                    </TestCase>
+
+                    <TestCase
+                        tags={['C_API']}
+                        itShould="Tester Shadows Example(fillOpacity)">
+                        <View style={styles.container}>
+                            <Shadow
+                                style={{
+                                    fillOpacity: 0.2,
+                                    stopColor: "#f0f0f0",//渐变结束的颜色
+                                    startColor: "#FF3A3A",//渐变开始的颜色
+                                    width: 110,
+                                    height: 110,
+                                    alignItems: "center",
+                                    justifyContent: "center"
+                                }}>
+                                <View
+                                    style={{
+                                        borderRadius: 25,
+                                        backgroundColor: "#FF3A3A",
+                                        width: 110,
+                                        height: 44,
+                                        alignItems: "center",
+                                        justifyContent: "center",
+                                    }}>
+                                    <Text
+                                        style={{
+                                            color: "#ffffff",
+                                            fontWeight: "bold",
+                                            textAlign: "center",
+                                            fontSize: 16
+                                        }}>
+                                        登录/注册
+                                    </Text>
+                                </View>
+                            </Shadow>
+                        </View>
+                    </TestCase>
+                    <TestCase
+                        tags={['C_API']}
+                        itShould="Tester Shadows Example(startColor)">
+                        <View style={styles.container}>
+                            <Shadow
+                                style={{
+                                    fillOpacity: 0.6,
+                                    stopColor: "#f0f0f0",//渐变结束的颜色
+                                    startColor: "blue",//渐变开始的颜色
+                                    width: 110,
+                                    height: 110,
+                                    alignItems: "center",
+                                    justifyContent: "center"
+                                }}>
+                                <View
+                                    style={{
+                                        borderRadius: 25,
+                                        backgroundColor: "#FF3A3A",
+                                        width: 110,
+                                        height: 44,
+                                        alignItems: "center",
+                                        justifyContent: "center",
+                                    }}>
+                                    <Text
+                                        style={{
+                                            color: "#ffffff",
+                                            fontWeight: "bold",
+                                            textAlign: "center",
+                                            fontSize: 16
+                                        }}>
+                                        登录/注册
+                                    </Text>
+                                </View>
+                            </Shadow>
+                        </View>
+                    </TestCase>
+                    <TestCase
+                        tags={['C_API']}
+                        itShould="Tester Shadows Example(stopColor)">
+                        <View style={styles.container}>
+                            <Shadow
+                                style={{
+                                    fillOpacity: 0.6,
+                                    stopColor: "blue",//渐变结束的颜色
+                                    startColor: "#FF3A3A",//渐变开始的颜色
+                                    width: 110,
+                                    height: 110,
+                                    alignItems: "center",
+                                    justifyContent: "center"
+                                }}>
+                                <View
+                                    style={{
+                                        borderRadius: 25,
+                                        backgroundColor: "#FF3A3A",
+                                        width: 110,
+                                        height: 44,
+                                        alignItems: "center",
+                                        justifyContent: "center",
+                                    }}>
+                                    <Text
+                                        style={{
+                                            color: "#ffffff",
+                                            fontWeight: "bold",
+                                            textAlign: "center",
+                                            fontSize: 16
+                                        }}>
+                                        登录/注册
+                                    </Text>
+                                </View>
+                            </Shadow>
+                        </View>
+                    </TestCase>
+                </TestSuite>
+            </Tester>
+        </ScrollView>
     );
 };
 
@@ -58,3 +167,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#f0f0f0',
     },
 });
+
+export default ShadowsDemo
