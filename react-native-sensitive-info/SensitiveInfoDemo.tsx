50044c2dd374d1050569b28f74f112013a18e25d
s4210110 <59352578+s4210110@users.noreply.github.com>
2024-07-17 16:54:07 +0800

    test: 修改react-native-sensitive-info测试demo (#438)
    Co-authored-by: ShiLong <n036459@archermind.com>

--- react-native-sensitive-info/SensitiveInfoDemo.tsx
+++ react-native-sensitive-info/SensitiveInfoDemo.tsx
@@ -1,11 +1,11 @@
 import React, { useCallback, useState } from 'react';
-import { TestSuite, Tester, Filter } from '@rnoh/testerino';
-import { View, Alert, Button, ColorValue, } from 'react-native';
-import { TestCase } from '../components';
-import sensitive from '@react-native-oh-tpl/react-native-sensitive-info'
-import { boolean } from 'yargs';
+import { TestSuite, Tester, TestCase } from '@rnoh/testerino';
+import { View, Alert, Button, ScrollView, Text, StyleSheet } from 'react-native';
+import sensitive from 'react-native-sensitive-info'
 
 export function SensitiveInfo() {
+  const [resTest, setResTest] = useState('');
+  const [isEnrollment, setIsEnrollment] = useState(false);
   const handleSetItem = useCallback((key: string, value: string) => {
 
     sensitive.setItem(key, value, {
@@ -20,19 +20,10 @@ export function SensitiveInfo() {
       sharedPreferencesName: 'exampleApp',
       keychainService: 'exampleApp',
     }).then(res => {
-      Alert.alert(res)
+      setResTest(res)
     });
   }, []);
 
-  const handleHasItem = useCallback(() => {
-    sensitive.hasItem('key1', {
-      sharedPreferencesName: 'exampleApp',
-      keychainService: 'exampleApp',
-      kSecAccessControl: 'kSecAccessControlBiometryAny', // Enabling FaceID
-      touchID: true,
-      showModal: true,
-    });
-  }, []);
 
   const handleDelItem = useCallback(() => {
     sensitive.deleteItem('key1', {
@@ -45,276 +36,280 @@ export function SensitiveInfo() {
     sensitive.getAllItems({
       sharedPreferencesName: 'exampleApp',
       keychainService: 'exampleApp',
-    }).then(res=>{Alert.alert(JSON.stringify(res))});
+    }).then(res => { setResTest(JSON.stringify(res)) });
   }, [])
 
   const handleHasEnrolledFingerprints = useCallback(() => {
-    sensitive.hasEnrolledFingerprints();
+    sensitive.hasEnrolledFingerprints().then(res => {
+      if (res) {
+        setResTest('true')
+      } else {
+        setResTest('false')
+      }
+    })
   }, [])
 
   const handleIsSensorAvailable = useCallback(() => {
     sensitive.isSensorAvailable().then(res => {
-      Alert.alert(JSON.stringify(res))
+      if (res.result == '12500000') {
+        setResTest('true')
+      } else {
+        setResTest('false')
+      }
     });
   }, [])
 
-  const handlecancelFingerprintAuth = (() => {
+  const handlecancelFingerprintAuth = useCallback(() => {
     try {
-
       sensitive.cancelFingerprintAuth()
     } catch {
       Alert.alert("Error")
     }
-  });
+  },[]);
 
-  const setInvalidatedByBiometricEnrollment = (() => {
-    try {
-
-      sensitive.setInvalidatedByBiometricEnrollment(false)
-    } catch {
-      Alert.alert("Error")
-    }
-  });
-
+  const InvalidatedByBiometricEnrollment = useCallback(() => {
+    sensitive.setInvalidatedByBiometricEnrollment(true);
+    setIsEnrollment(!isEnrollment)
+    setResTest(JSON.stringify(isEnrollment))
+  },[]);
 
   return (
-    <Tester>
-      <TestSuite name='SensitiveInfoDemo'>
-        <TestCase.Manual
-          itShould="存入键值对key:key1 value:value1"
-          tags={["dev"]}
-          initialState={false}
-          arrange={({ setState }) =>
-            <Button onPress={async () => {
-              try {
-                await handleSetItem('key1', 'value1')
-                setState(true)
-              } catch {
-                setState(false)
-              }
+    <Tester style={{ height: '100%' }}>
+      <View style={styles.content}>
+        <Text style={styles.contentTest}>
+          {resTest}
+        </Text>
+      </View>
+      <ScrollView >
+        <TestSuite name='SensitiveInfoDemo' >
+
+          <TestCase
+            itShould="存入键值对key:key1 value:value1"
+            tags={["dev"]}
+            initialState={false}
+            arrange={({ setState }) =>
+              <Button onPress={async () => {
+                try {
+                  await handleSetItem('key1', 'value1')
+                  setState(true)
+                } catch {
+                  setState(false)
+                }
+              }} title={'Add item using setItem'}></Button>
+            }
+            assert={({ expect, state }) => {
+              expect(state).to.be.eq(true);
+            }}
+          />
+
+          <TestCase
+            itShould="存入键值对key:key2 value:value2"
+            tags={["dev"]}
+            initialState={false}
+            arrange={({ setState }) =>
+              <Button onPress={async () => {
+                try {
+                  await handleSetItem('key2', 'value2')
+                  setState(true)
+                } catch {
+                  setState(false)
+                }
 
 
-            }} title={'Add item using setItem'}></Button>
-          }
-          assert={({ expect, state }) => {
-            expect(state).to.be.eq(true);
-          }}
-        />
-
-        <TestCase.Manual
-          itShould="存入键值对key:key2 value:value2"
-          tags={["dev"]}
-          initialState={false}
-          arrange={({ setState }) =>
-            <Button onPress={async () => {
-              try {
-                await handleSetItem('key2', 'value2')
-                setState(true)
-              } catch {
-                setState(false)
-              }
+              }} title={'Add item using setItem'}></Button>
+            }
+            assert={({ expect, state }) => {
+              expect(state).to.be.eq(true);
+            }}
+          />
 
 
-            }} title={'Add item using setItem'}></Button>
-          }
-          assert={({ expect, state }) => {
-            expect(state).to.be.eq(true);
-          }}
-        />
+          <TestCase
+            itShould="取出键值对value1"
+            tags={["dev"]}
+            initialState={false}
+            arrange={({ setState }) =>
+              <Button onPress={async () => {
+                try {
+                  await handleGetItem('key1')
+                  setState(true)
+                } catch {
+                  setState(false)
+                }
 
 
-        <TestCase.Manual
-          itShould="取出键值对value1"
-          tags={["dev"]}
-          initialState={false}
-          arrange={({ setState }) =>
-            <Button onPress={async () => {
-              try {
-                await handleGetItem('key1')
-                setState(true)
-              } catch {
-                setState(false)
-              }
+              }} title={'Add item using getItem'}></Button>
+            }
+            assert={({ expect, state }) => {
+              expect(state).to.be.eq(true);
+            }}
+          />
+
+          <TestCase
+            itShould="取出键值对key2"
+            tags={["dev"]}
+            initialState={false}
+            arrange={({ setState }) =>
+              <Button onPress={async () => {
+                try {
+                  await handleGetItem('key2')
+                  setState(true)
+                } catch {
+                  setState(false)
+                }
 
 
-            }} title={'Add item using getItem'}></Button>
-          }
-          assert={({ expect, state }) => {
-            expect(state).to.be.eq(true);
-          }}
-        />
-
-        <TestCase.Manual
-          itShould="取出键值对key2"
-          tags={["dev"]}
-          initialState={false}
-          arrange={({ setState }) =>
-            <Button onPress={async () => {
-              try {
-                await handleGetItem('key2')
-                setState(true)
-              } catch {
-                setState(false)
-              }
+              }} title={'Add item using getItem'}></Button>
+            }
+            assert={({ expect, state }) => {
+              expect(state).to.be.eq(true);
+            }}
+          />
 
 
-            }} title={'Add item using getItem'}></Button>
-          }
-          assert={({ expect, state }) => {
-            expect(state).to.be.eq(true);
-          }}
-        />
 
-        <TestCase.Manual
-          itShould="判断键值对是否存在"
-          tags={["dev"]}
-          initialState={false}
-          arrange={({ setState }) =>
-            <Button onPress={async () => {
-              try {
-                await handleHasItem()
-                setState(true)
-              } catch {
-                setState(false)
-              }
+          <TestCase
+            itShould="删除'key1'"
+            tags={["dev"]}
+            initialState={false}
+            arrange={({ setState }) =>
+              <Button onPress={async () => {
+                try {
+                  await handleDelItem()
+                  setState(true)
+                } catch {
+                  setState(false)
+                }
 
 
-            }} title={'Add item using handleSetItemUsingTouchIDOnPress'}></Button>
-          }
-          assert={({ expect, state }) => {
-            expect(state).to.be.eq(true);
-          }}
-        />
+              }} title={'Add item using deleteItem'}></Button>
+            }
+            assert={({ expect, state }) => {
+              expect(state).to.be.eq(true);
+            }}
+          />
 
 
-        <TestCase.Manual
-          itShould="删除键值对"
-          tags={["dev"]}
-          initialState={false}
-          arrange={({ setState }) =>
-            <Button onPress={async () => {
-              try {
-                await handleDelItem()
-                setState(true)
-              } catch {
-                setState(false)
-              }
+          <TestCase
+            itShould="获取全部键值对"
+            tags={["dev"]}
+            initialState={false}
+            arrange={({ setState }) =>
+              <Button onPress={async () => {
+                try {
+                  await handleGetAllItems()
+                  setState(true)
+                } catch {
+                  setState(false)
+                }
 
 
-            }} title={'Add item using deleteItem'}></Button>
-          }
-          assert={({ expect, state }) => {
-            expect(state).to.be.eq(true);
-          }}
-        />
+              }} title={'Add item using handleGetAllItems'}></Button>
+            }
+            assert={({ expect, state }) => {
+              expect(state).to.be.eq(true);
+            }}
+          />
+
+          <TestCase
+            itShould="判断指纹解锁是否可用"
+            tags={["dev"]}
+            initialState={false}
+            arrange={({ setState }) =>
+              <Button onPress={async () => {
+                try {
+                  await handleHasEnrolledFingerprints()
+                  setState(true)
+                } catch {
+                  setState(false)
+                }
 
 
-        <TestCase.Manual
-          itShould="获取全部键值对"
-          tags={["dev"]}
-          initialState={false}
-          arrange={({ setState }) =>
-            <Button onPress={async () => {
-              try {
-                await handleGetAllItems()
-                setState(true)
-              } catch {
-                setState(false)
-              }
+              }} title={'Add item using handleHasEnrolledFingerprints'}></Button>
+            }
+            assert={({ expect, state }) => {
+              expect(state).to.be.eq(true);
+            }}
+          />
+
+          <TestCase
+            itShould="获取指纹解锁权限"
+            tags={["dev"]}
+            initialState={false}
+            arrange={({ setState }) =>
+              <Button onPress={async () => {
+                try {
+                  await handleIsSensorAvailable()
+                  setState(true)
+                } catch {
+                  setState(false)
+                }
 
 
-            }} title={'Add item using handleGetAllItems'}></Button>
-          }
-          assert={({ expect, state }) => {
-            expect(state).to.be.eq(true);
-          }}
-        />
+              }} title={'Add item using handleIsSensorAvailable'}></Button>
+            }
+            assert={({ expect, state }) => {
+              expect(state).to.be.eq(true);
+            }}
+          />
 
-        <TestCase.Manual
-          itShould="判断是否已注册指纹"
-          tags={["dev"]}
-          initialState={false}
-          arrange={({ setState }) =>
-            <Button onPress={async () => {
-              try {
-                await handleHasEnrolledFingerprints()
-                setState(true)
-              } catch {
-                setState(false)
-              }
-
-
-            }} title={'Add item using handleHasEnrolledFingerprints'}></Button>
-          }
-          assert={({ expect, state }) => {
-            expect(state).to.be.eq(true);
-          }}
-        />
-
-        <TestCase.Manual
-          itShould="判断指纹解锁是否可用"
-          tags={["dev"]}
-          initialState={false}
-          arrange={({ setState }) =>
-            <Button onPress={async () => {
-              try {
-                await handleIsSensorAvailable()
-                setState(true)
-              } catch {
-                setState(false)
-              }
+          <TestCase
+            itShould="取消指纹认证"
+            tags={["dev"]}
+            initialState={false}
+            arrange={({ setState }) =>
+              <Button onPress={async () => {
+                try {
+                  await handlecancelFingerprintAuth()
+                  setState(true)
+                } catch {
+                  setState(false)
+                }
 
 
-            }} title={'Add item using handleIsSensorAvailable'}></Button>
-          }
-          assert={({ expect, state }) => {
-            expect(state).to.be.eq(true);
-          }}
-        />
+              }} title={'Add item using handlecancelFingerprintAuth'}></Button>
+            }
+            assert={({ expect, state }) => {
+              expect(state).to.be.eq(true);
+            }}
+          />
 
-        <TestCase.Manual
-          itShould="取消指纹认证"
-          tags={["dev"]}
-          initialState={false}
-          arrange={({ setState }) =>
-            <Button onPress={async () => {
-              try {
-                await handlecancelFingerprintAuth()
-                setState(true)
-              } catch {
-                setState(false)
-              }
-
-
-            }} title={'Add item using handlecancelFingerprintAuth'}></Button>
-          }
-          assert={({ expect, state }) => {
-            expect(state).to.be.eq(true);
-          }}
-        />
-
-
-        <TestCase.Manual
-          itShould="设置无效的通过生物识别注册"
-          tags={["dev"]}
-          initialState={false}
-          arrange={({ setState }) =>
-            <Button onPress={async () => {
-              try {
-                await setInvalidatedByBiometricEnrollment()
-                setState(true)
-              } catch {
-                setState(false)
-              }
-
-
-            }} title={'Add item using setInvalidatedByBiometricEnrollment'}></Button>
-          }
-          assert={({ expect, state }) => {
-            expect(state).to.be.eq(true);
-          }}
-        />
-      </TestSuite>
+          <TestCase
+            itShould="关闭指纹权限"
+            tags={["dev"]}
+            initialState={false}
+            arrange={({ setState }) =>
+              <Button onPress={async () => {
+                try {
+                  await sensitive.setInvalidatedByBiometricEnrollment(true)
+                  setState(true)
+                } catch {
+                  setState(false)
+                }
+              }} title={'Add item using handlecancelFingerprintAuth'}></Button>
+            }
+            assert={({ expect, state }) => {
+              expect(state).to.be.eq(true);
+            }}
+          />
+        </TestSuite>
+      </ScrollView>
     </Tester>
   );
 }
+const styles = StyleSheet.create({
+  content: {
+    position: 'absolute',
+    top: 50,
+    width: '100%',
+    height: 'auto',
+    zIndex: 10,
+    backgroundColor: '#fff',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  contentTest: {
+    marginTop: 10,
+    marginLeft: 10,
+  }
+});
