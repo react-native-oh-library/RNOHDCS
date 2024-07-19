40abfe71c6cf535e7ad93f683a3778d945374129
zy432099 <46698434+zy432099@users.noreply.github.com>
2024-07-19 10:55:15 +0800

    feat:新增react-native-offline测试demo (#471)
    Co-authored-by: 01xkp <1903679953@qq.com>

---
+++ react-native-offline/screens/SagasScreen.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { Image, Platform, StyleSheet, View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import ReduxNetworkReader from '../components/ReduxNetworkReader';
+import Counter from '../components/Counter';
+import ActionButtons from '../components/ActionButtons';
+import OfflineQueue from '../components/OfflineQueue';
+import createStore from '../redux/createStore';
+import { Tester, TestSuite, TestCase } from '@rnoh/testerino';
+
+const store = createStore({ withSaga: true, queueReleaseThrottle: 250 });
+
+export class SagasScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Sagas',
+  };
+
+  render() {
+    return (
+      <Tester>
+        <TestSuite name="SagasTest">
+          <TestCase tags={['C_API']} itShould="SagasTest"
+            initialState={''}
+            arrange={({ setState }) =>
+              <Provider store={store}>
+                <View style={styles.container}>
+                  <View style={styles.imageContainer}>
+                    <Image
+                      source={require('../assets/images/redux-saga.png')}
+                      style={styles.image}
+                    />
+                  </View>
+                  <View style={styles.firstSection}>
+                    <Text
+                      style={{
+                        marginTop: 10,
+                        fontSize: 17,
+                        color: 'purple',
+                        marginBottom: 30,
+                        textAlign: 'center',
+                      }}
+                    >
+                      For sagas example, you have to disconnect your Internet manually
+                    </Text>
+                    <ReduxNetworkReader />
+                    <Counter />
+                  </View>
+                  <View style={styles.secondSection}>
+                    <ActionButtons />
+                    <View style={styles.offlineQueue}>
+                      <OfflineQueue title="Offline Queue (FIFO), throttle = 250ms" />
+                    </View>
+                  </View>
+                </View>
+              </Provider>
+            }
+            assert={({ expect, state }) => {
+              expect(state).to.be.eq('success');
+            }}>
+          </TestCase>
+        </TestSuite>
+      </Tester>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 30,
+  },
+  imageContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  image: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  firstSection: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  secondSection: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  offlineQueue: {
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3, width: 1 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    backgroundColor: '#fbfbfb',
+    paddingTop: 10,
+  },
+});
