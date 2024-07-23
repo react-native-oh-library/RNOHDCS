ae717c80d0d1bd6fed45d0d18a63537107050e99
goods01 <2047190290@qq.com>
2024-07-23 18:58:16 +0800

    test:修改react-native-action-button组件demo

--- react-native-action-button/ActionButtonDemo.tsx
+++ react-native-action-button/ActionButtonDemo.tsx
@@ -47,8 +47,8 @@ export const ActionBtnTest = () => {
   const [itemOpcity, setOpcity] = useState(0)
   const [itemHide, setItemHide] = useState(false)
   const [itemShadow, setItemShadow] = useState("red")
-
-
+  const [resetToken,setRestToken] = useState(1)
+  const [backgroundTappable,setBackgroundTappable] =useState(false)
 
   const styles = StyleSheet.create({
     btnShow: {
@@ -69,8 +69,8 @@ export const ActionBtnTest = () => {
       <ScrollView >
         <TestSuite name="react-native-action-button">
           <TestCase
-            key={`getInitStatus_1`}
-            itShould={"改变外侧按钮的大小"}
+            key={`getInitStatus_1  `}
+            itShould={"改变外侧按钮的大小  size属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -102,10 +102,81 @@ export const ActionBtnTest = () => {
               expect(state).to.be.true;
             }}
           />
+         <TestCase
+            key={`getInitStatus_30`}
+            itShould={"将active button 折叠 点击active按钮会重新渲染展开子按钮   resetToken属性"}
+            tags={['C_API']}
+            initialState={false}
+            arrange={({ setState }) => {
+              return (
+                <View style={{ flex: 1,height:200 }}>
+                  {
+                    <ActionButton active={true} resetToken={resetToken}>
+                    <ActionButton.Item
+                      buttonColor="#9b59b6"
+                      title="New Task"
+                      onPress={() => console.log("notes tapped!")}
+                    >
+                      <Text>1</Text>
+                     
+                    </ActionButton.Item>
+                    <ActionButton.Item
+                      buttonColor="#3498db"
+                      title="Notifications"
+                    >
+                      <Text>2</Text>
+                      
+                    </ActionButton.Item>
+                  </ActionButton>
+                  }
+                  <Button title={"active"} onPress={() => {
+                    setRestToken(2)
+                    setActive(false)
+                    setState(true)
+                  }}></Button>
+                </View>
+              );
+            }}
+            assert={async ({ expect, state }) => {
+              expect(state).to.be.true;
+            }}
+          />
+                <TestCase
+            key={`getInitStatus_31`}
+            itShould={"ActionButton展开时背景是否可以点击  ActionButton backgroundTappable属性"}
+            tags={['C_API']}
+            initialState={false}
+            arrange={({ setState }) => {
+              return (
+                <View style={{ flex: 1,height:200 }}>
+                   {
+                  <ActionButton  bgColor="green" backgroundTappable={backgroundTappable} >
+                       <ActionButton.Item
+                      size={itemSize}
+                      buttonColor="#9b59b6"
+                      title={itemTitle}
+                     
+                    >
+                      <Text>1</Text>
+                    </ActionButton.Item>
+                    </ActionButton>
+                   }
+                  <Button title={"active"} onPress={() => {
+                  setBackgroundTappable(true)
+                    setState(true)
+                
+                  }}></Button>
+                </View>
+              );
+            }}
+            assert={async ({ expect, state }) => {
+              expect(state).to.be.true;
+            }}
+          />
           
           <TestCase
             key={`getInitStatus_2`}
-            itShould={"改变悬浮按钮的状态"}
+            itShould={"改变悬浮按钮的状态   active属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -148,7 +219,7 @@ export const ActionBtnTest = () => {
 
           <TestCase
             key={`getInitStatus_3`}
-            itShould={"改变按钮的位置"}
+            itShould={"改变按钮的位置  position属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -184,7 +255,7 @@ export const ActionBtnTest = () => {
           />
            <TestCase
             key={`getInitStatus_4`}
-            itShould={"按钮阴影是否显示"}
+            itShould={"按钮阴影是否显示  hideShadows属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -220,7 +291,7 @@ export const ActionBtnTest = () => {
           />
           <TestCase
             key={`getInitStatus_5`}
-            itShould={"设置ActionButton的的颜色"}
+            itShould={"设置ActionButton的的颜色  buttonColor属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -248,7 +319,7 @@ export const ActionBtnTest = () => {
           />
            <TestCase
             key={`getInitStatus_6`}
-            itShould={"子按钮展开时的背景色透明度"}
+            itShould={"子按钮展开时的背景色透明度   bgOpacity属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -277,7 +348,7 @@ export const ActionBtnTest = () => {
 
           <TestCase
             key={`getInitStatus_7`}
-            itShould={"子按钮展开时的背景色"}
+            itShould={"子按钮展开时的背景色  bgColor属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -304,7 +375,7 @@ export const ActionBtnTest = () => {
           />
           <TestCase
             key={`getInitStatus_8`}
-            itShould={"按钮X轴的偏移量"}
+            itShould={"按钮X轴的偏移量  offsetX属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -332,7 +403,7 @@ export const ActionBtnTest = () => {
           />
           <TestCase
             key={`getInitStatus_9`}
-            itShould={"按钮Y轴的偏移量"}
+            itShould={"按钮Y轴的偏移量  offsetY属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -360,7 +431,7 @@ export const ActionBtnTest = () => {
           />
           <TestCase
             key={`getInitStatus_10`}
-            itShould={"设置ActionButton子按钮之间的间距 "}
+            itShould={"设置ActionButton子按钮之间的间距  spacing属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -395,7 +466,7 @@ export const ActionBtnTest = () => {
           />
           <TestCase
             key={`getInitStatus_11`}
-            itShould={"设置ActionButton的文字"}
+            itShould={"设置ActionButton的文字  buttonText属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -423,7 +494,7 @@ export const ActionBtnTest = () => {
           />
            <TestCase
             key={`getInitStatus_12`}
-            itShould={"按下ActionButton的子按钮是否自动隐藏"}
+            itShould={"按下ActionButton的子按钮是否自动隐藏  autoInactive属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -459,7 +530,7 @@ export const ActionBtnTest = () => {
           />
             <TestCase
             key={`getInitStatus_13`}
-            itShould={"设置ActionButton子按钮展开的方向(up/down) "}
+            itShould={"设置ActionButton子按钮展开的方向(up/down)  verticalOrientation属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -487,7 +558,7 @@ export const ActionBtnTest = () => {
           />
            <TestCase
             key={`getInitStatus_14`}
-            itShould={"设置ActionButton的文字样式"}
+            itShould={"设置ActionButton的文字样式 buttonTextStyle属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -515,7 +586,7 @@ export const ActionBtnTest = () => {
           />
            <TestCase
             key={`getInitStatus_15`}
-            itShould={"ActionButton按下透明度的改变"}
+            itShould={"ActionButton按下透明度的改变  activeOpacity属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -536,7 +607,7 @@ export const ActionBtnTest = () => {
           />
             <TestCase
             key={`getInitStatus_16`}
-            itShould={"点击事件"}
+            itShould={"点击事件  onPress属性"  }
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -559,7 +630,7 @@ export const ActionBtnTest = () => {
           />
             <TestCase
             key={`getInitStatus_17`}
-            itShould={"图标旋转角度的改变"}
+            itShould={"图标旋转角度的改变  degrees属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -595,7 +666,7 @@ export const ActionBtnTest = () => {
           />
          <TestCase
             key={`getInitStatus_18`}
-            itShould={"子按钮展开外层按钮文字的变化"}
+            itShould={"子按钮展开外层按钮文字的变化   renderIcon属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -633,7 +704,7 @@ export const ActionBtnTest = () => {
           />
              <TestCase
             key={`getInitStatus_20`}
-            itShould={"阴影颜色的变化"}
+            itShould={"阴影颜色的变化   shadowStyle属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -666,7 +737,7 @@ export const ActionBtnTest = () => {
           />
            <TestCase
             key={`getInitStatus_21`}
-            itShould={"子按钮大小的改变"}
+            itShould={"子按钮大小的改变  ActionButton.Item size属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -697,7 +768,7 @@ export const ActionBtnTest = () => {
           />
              <TestCase
             key={`getInitStatus_22`}
-            itShould={"子按钮旁边文字的改变"}
+            itShould={"子按钮旁边文字的改变  ActionButton.Item title属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -709,7 +780,6 @@ export const ActionBtnTest = () => {
                       size={itemSize}
                       buttonColor="#9b59b6"
                       title={itemTitle}
-                      onPress={() => console.log("notes tapped!")}
                     >
                       <Text>1</Text>
                     </ActionButton.Item>
@@ -728,7 +798,7 @@ export const ActionBtnTest = () => {
           />
            <TestCase
             key={`getInitStatus_23`}
-            itShould={"点击事件"}
+            itShould={"子按钮点击事件  ActionButton.Item onPress属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -759,7 +829,7 @@ export const ActionBtnTest = () => {
           />
            <TestCase
             key={`getInitStatus_24`}
-            itShould={"子按钮文字样式"}
+            itShould={"子按钮文字样式  ActionButton.Item textStyle属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -792,7 +862,7 @@ export const ActionBtnTest = () => {
             
             <TestCase
             key={`getInitStatus_25`}
-            itShould={"子按钮文字容器的样式"}
+            itShould={"子按钮文字容器的样式  ActionButton.Item textContainerStyle属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -824,7 +894,7 @@ export const ActionBtnTest = () => {
           />
           <TestCase
             key={`getInitStatus_26`}
-            itShould={"子按钮与文本之间的间距"}
+            itShould={"子按钮与文本之间的间距  ActionButton.Item spaceBetween属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -864,7 +934,7 @@ export const ActionBtnTest = () => {
           />
            <TestCase
             key={`getInitStatus_27`}
-            itShould={"点击子按钮背景透明度的改变"}
+            itShould={"点击子按钮背景透明度的改变   ActionButton.Item activeOpacity属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -873,12 +943,12 @@ export const ActionBtnTest = () => {
                   {
                     <ActionButton autoInactive={false} >
                       <ActionButton.Item
-                        size={itemSize}
+                        size={60}
                         buttonColor="#9b59b6"
                         title={itemTitle}
-                        // activeOpacity={itemOpcity}
                         activeOpacity={itemOpcity}
                         onPress={() => { }}
+                        
                       >
                         <Text>1</Text>
                       </ActionButton.Item>
@@ -898,7 +968,7 @@ export const ActionBtnTest = () => {
           />
           <TestCase
             key={`getInitStatus_28`}
-            itShould={"展示与隐藏子按钮阴影"}
+            itShould={"展示与隐藏子按钮左侧文字的阴影   ActionButton.Item hideLabelShadow属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -937,7 +1007,7 @@ export const ActionBtnTest = () => {
 
           <TestCase
             key={`getInitStatus_29`}
-            itShould={"子按钮阴影的改变"}
+            itShould={"子按钮阴影的改变  ActionButton.Item shadowStyle属性"}
             tags={['C_API']}
             initialState={false}
             arrange={({ setState }) => {
@@ -967,6 +1037,7 @@ export const ActionBtnTest = () => {
               expect(state).to.be.true;
             }}
           />
+     
           
         </TestSuite>
       
