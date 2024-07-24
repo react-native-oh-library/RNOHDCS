f748313430bf0daae983f9f996ac8c0853a597fe
wangjinygithub <80186328+wangjinygithub@users.noreply.github.com>
2024-07-24 15:13:24 +0800

    test: 修改react-native-modal-popover示例 (#572)

--- react-native-modal-popover/PopoverOnePopoverExample.tsx
+++ react-native-modal-popover/PopoverOnePopoverExample.tsx
@@ -50,7 +50,7 @@ export function PopoverOnePopoverExample() {
           closePopover?.()
         } : closePopover}
         fromRect={popoverAnchorRect}
-        supportedOrientations={['portrait']}
+        supportedOrientations={['portrait', 'landscape']}
         {...prpos.prop}
       >
         <Text>Hello from inside popover!</Text>
@@ -181,14 +181,6 @@ export function PopoverOnePopoverExample() {
     {
       prop: { useNativeDriver: true },
       name: '自定义useNativeDriver(true)',
-    },
-    {
-      prop: { supportedOrientations: ['portrait-upside-down', 'landscape-left'] },
-      name: '自定义supportedOrientations1',
-    },
-    {
-      prop: { supportedOrientations: ['portrait-upside-down'] },
-      name: '自定义supportedOrientations2',
     }
   ]
 
