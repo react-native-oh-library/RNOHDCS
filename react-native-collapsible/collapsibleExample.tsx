ac5560fac2d9758e61b2587398337bae9c340c7f
cmdssd <168167043+cmdssd@users.noreply.github.com>
2024-07-25 18:39:20 +0800

    test:修改react-native-collapsible测试demo (#610)
    Co-authored-by: kongchuiyu <kongchuiyu@h.partners.com>

--- react-native-collapsible/collapsibleExample.tsx
+++ react-native-collapsible/collapsibleExample.tsx
@@ -13,6 +13,7 @@ import {TestSuite, Tester, TestCase} from '@rnoh/testerino';
 import Collapsible, {EasingMode} from 'react-native-collapsible';
 import Accordion from 'react-native-collapsible/Accordion';
 
+let time :any;
 export function CollapsibleExample() {
   const [activeSections, setActiveSections] = useState([0]);
   const [collapsed, setCollapsed] = useState(true);
@@ -22,20 +23,27 @@ export function CollapsibleExample() {
   const renderContentRef = useRef('');
   const renderFeetRef = useRef('');
   const renderSectionTitleRef = useRef('');
+  // const buttonEvent = useRef('');
+  const [buttonEvent,setButtonEvent] = useState('');
+
   const [data, setData] = useState({
     underlayColor:true,
     expandMultiple: true,
     duration: true,
-    renderAsFlatList: false,
+    renderAsFlatList: true,
     keyExtractor: true,
     disabled: true,
     align: 'top',
     easing: 'linear',
     collapsedHeight: 0,
+    enablePointerEvents:true,
     renderChildrenCollapsed: false,
     onAnimationEnd: '',
     sections: true,
     touchableComponent: true,
+    touchableProps:{},
+    sectionContainerStyle:{ },
+    containerStyle:{ backgroundColor: '#F500FF'},
   });
 
   function toggleExpanded() {
@@ -57,7 +65,7 @@ export function CollapsibleExample() {
   function renderHeader(section: any, _: any, isActive: any) {
     renderHeaderRef.current = 'pass'
     return (
-      <View style={[styles.header, isActive ? styles.active : styles.inactive]}>
+      <View>
         <Pressable onPress={() => { setSections(_); }}>
           <Text style={styles.headerText}>{section.title}</Text>
         </Pressable>
@@ -67,22 +75,17 @@ export function CollapsibleExample() {
 
   function renderContent(section: any, _: any, isActive: any) {
     renderContentRef.current = 'pass'
-    return (
-      <View
-        style={[styles.content, isActive ? styles.active : styles.inactive]}>
-        <Text> {section.content} </Text>
-      </View>
-    );
+    return (<View><Text>{section.content}</Text></View>);
   }
 
   function renderSectionTitle(section: any, _: any, isActive: any) {
     renderSectionTitleRef.current = 'pass'
-    return ( <View style = {{backgroundColor:'white'}}> <Text> {section.SectionTitle} </Text> </View> );
+    return ( <View><Text>{section.SectionTitle}</Text></View> );
   }
 
   function renderFooter(section: any, _: any, isActive: any) {
     renderFeetRef.current = 'pass'
-    return ( <View style = {{backgroundColor:'white'}}> <Text> {section.feet}</Text></View> );
+    return ( <View><Text>{section.feet}</Text></View> );
   }
   function tester_change(title:string,buttonTitle:string,pressEvent:Function){
       return(          
@@ -93,11 +96,10 @@ export function CollapsibleExample() {
       )
   }
 
-
   return (
     <ScrollView>
       <Tester>
-        <TestSuite name="Collapsible">
+        {/* <TestSuite name="Collapsible">
           <TestCase itShould="单项目展开" >
               <TouchableOpacity onPress={toggleExpanded}>
                 <View style={styles.header}>
@@ -111,19 +113,32 @@ export function CollapsibleExample() {
             onAnimationEnd={() => { setData({...data, onAnimationEnd: 'pass'}); }}
             duration={data.duration ? 400 : 4000}
             collapsedHeight={data.collapsedHeight}
+            enablePointerEvents={data.enablePointerEvents}
             renderChildrenCollapsed={data.renderChildrenCollapsed}
             >
             <View style={styles.content}>
-              <Text>
-                {' '}
-                Bacon ipsum dolor amet chuck turducken landjaeger tongue spare
-                ribs{' '}
-              </Text>
+              <View style={{backgroundColor:'red',height:'100'}}> <Text>{' '} Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs{buttonEvent+' '}</Text> </View>
+              <Button title='这是一个按钮' onPress={()=>{ 
+                  setButtonEvent('点击') 
+                  if(time){
+                    clearInterval(time);
+                  }
+                  time = setInterval(()=>{
+                    setButtonEvent('')
+                  },1000);
+                }}></Button>
             </View>
           </Collapsible>
           
           {tester_change("展开动画锚点 align: 'top'",'top',()=>{ setData({...data, align: 'top'}) })}
-          {tester_change("展开动画锚点 align: 'center'",'center',()=>{ setData({...data, align: 'center'}) })}
+          {tester_change("展开动画锚点 align: 'center'",'center',()=>{ setData({...data, align: 'bottom'}) })}
+          
+          {tester_change("是否显示子组件 collapsed: true",'true',()=>{ setCollapsed(true); })}
+          {tester_change("是否显示子组件 collapsed: false",'false',()=>{ setCollapsed(false); })}
+          {tester_change("在折叠视图上启用指针事件 enablePointerEvents: true",'true',()=>{ setData({...data, enablePointerEvents:true});setCollapsed(true); })}
+          {tester_change("在折叠视图上启用指针事件 enablePointerEvents: false",'false',()=>{ setData({...data, enablePointerEvents:false});setCollapsed(true); })}
+
+
           {tester_change("折叠高度预留 collapsedHeight: 0",'0',()=>{ setData({...data, collapsedHeight: 0}) })}
           {tester_change("折叠高度预留 collapsedHeight: 40",'40',()=>{ setData({...data, collapsedHeight: 40}) })}
           {tester_change("强制渲染折叠的子项 renderChildrenCollapsed: true",'true',()=>{ setData({...data, renderChildrenCollapsed: true}) })}
@@ -133,13 +148,14 @@ export function CollapsibleExample() {
 
           <TestCase itShould="动画效果结束回调"> <Text> {data.onAnimationEnd} </Text> </TestCase>
           
-        </TestSuite>
+        </TestSuite> */}
         <TestSuite name="Accordion">
           <Accordion
             underlayColor = {data.underlayColor?'black':'red'}
             activeSections={activeSections}
             sections={data.sections ? CONTENT : CONTENT1}
             touchableComponent={ data.touchableComponent ? TouchableHighlight : TouchableOpacity }
+            touchableProps = {data.touchableProps}
             expandMultiple={data.expandMultiple}
             renderHeader={renderHeader}
             renderContent={renderContent}
@@ -147,19 +163,20 @@ export function CollapsibleExample() {
             renderFooter = {renderFooter}
             duration={data.duration ? 400 : 4000}
             expandFromBottom={expandFromBottom}
+            sectionContainerStyle = {data.sectionContainerStyle}
+            containerStyle = {data.containerStyle}
             onChange={setSections}
             renderAsFlatList={data.renderAsFlatList}
             disabled={data.disabled}
             easing={data.easing}
             keyExtractor={(item, index) => {
-              if (data.keyExtractor) {
+              if(data.keyExtractor){
                 return index;
-              } else {
-                return (index + 3) % 5;
+              }else{
+                return item.ekey
               }
             }}
           />
-
           <TestCase
             itShould="多项目展开"
             initialState={false}
@@ -177,7 +194,8 @@ export function CollapsibleExample() {
                           setActiveSections([]);
                         }
                         if (activeSections.length > 1) setState(true);
-                      }}>
+                      }
+                      }>
                       <View style={{ backgroundColor: '#F5FCFF', padding: 5,}}>
                         <Text
                           style={
@@ -195,6 +213,13 @@ export function CollapsibleExample() {
             )}
             assert={({expect, state}) => { expect(state).to.be.true; }}>
           </TestCase>
+          {tester_change("容器样式 containerStyle: {backgroundColor: '#F500FF'}",'{backgroundColor:#F500FF}',()=>{ setData({...data, containerStyle: { backgroundColor: '#F500FF'},sectionContainerStyle: { }}) })}
+          {tester_change("容器样式 containerStyle: {backgroundColor: '#00fff0'}",'{backgroundColor:#00fff0}',()=>{ setData({...data, containerStyle: { backgroundColor: '#00fff0'},sectionContainerStyle: { }}) })}
+          {tester_change("触摸组件属性 touchableProps: {style:{height:30}}",`{style:{height:30}}`,()=>{ setData({...data, touchableProps: {style:{height:30}}}) })}
+          {tester_change("触摸组件属性 touchableProps: {style:{}}",'{style:{}}',()=>{ setData({...data, touchableProps: {style:{}}}) })}
+          {tester_change("容器样式 sectionContainerStyle: {backgroundColor:#F500FF}",'{backgroundColor:#F500FF}',()=>{ setData({...data, sectionContainerStyle: { backgroundColor: '#0000FF'}}) })}
+          {tester_change("容器样式 sectionContainerStyle: {backgroundColor:#00fff0}",'{backgroundColor:#00fff0}',()=>{ setData({...data, sectionContainerStyle: { backgroundColor: '#54cc00'}}) })}
+
 
           {tester_change("展开动画锚点 align: 'top'",'top',()=>{setData({...data, align: 'top'})})}
           {tester_change("展开动画锚点 align: 'bottom'",'bottom',()=>{setData({...data, align: 'bottom'})})}
@@ -207,26 +232,25 @@ export function CollapsibleExample() {
           {tester_change("可交互 disabled: true",'可交互',()=>{ setData({...data, disabled: true}) })}
           {tester_change("不可交互 disabled: false",'不可交互',()=>{ setData({...data, disabled: false}) })}
           {tester_change("从底部/顶部展开 expandFromBottom:true",'顶部',()=>{ setExpandFromBottom(true) })}
-          {tester_change("从底部/顶部展开 expandFromBottom:false",'底部',()=>{ setExpandFromBottom(!false) })}
-
+          {tester_change("从底部/顶部展开 expandFromBottom:false",'底部',()=>{ setExpandFromBottom(false) })}
           {tester_change("多重展开 expandMultiple: true",'多重展开',()=>{ setData({...data, expandMultiple: true}) })}
           {tester_change("多重展开 expandMultiple: false",'单个展开',()=>{ setData({...data, expandMultiple: false}) })}
           {tester_change("展开时间 duration: true",'400ms',()=>{ setData({...data, duration: true}) })}
           {tester_change("展开时间 duration: false",'4000ms',()=>{ setData({...data, duration: false}) })}
           {tester_change("使用FlatList渲染 renderAsFlatList: true",'true',()=>{ setData({...data, renderAsFlatList: true}) })}
           {tester_change("使用FlatList渲染 renderAsFlatList: false",'false',()=>{ setData({...data, renderAsFlatList: false}) })}
-          {tester_change("内容数组排序",'true',()=>{ setData({...data, renderAsFlatList: true,keyExtractor:true}) })}
-          {tester_change("内容数组排序",'false',()=>{ setData({...data, renderAsFlatList: false,keyExtractor:true}) })}
+          {tester_change("keyExtractor true",'true',()=>{ setData({...data, keyExtractor: true,renderAsFlatList:true}) })}
+          {tester_change("keyExtractor false",'false',()=>{ setData({...data, keyExtractor: false,renderAsFlatList:true}) })}
 
           {tester_change("EasingMode easing: 'linear'",'linear',()=>{ setData({...data, easing: 'linear'}) })}
           {tester_change("EasingMode easing: 'easeInQuad'",'easeInQuad',()=>{ setData({...data, easing: 'easeInQuad'}) })}
 
 
-          <TestCase itShould="点击展开页面标题回调 onChange"> <Text>{renderHeaderRef.current}</Text> </TestCase>
-          <TestCase itShould="渲染子页面头 renderHeader"> <Text>{onChangeRef.current}</Text>  </TestCase>
-          <TestCase itShould="渲染子页面内容 renderContent"> <Text>{renderContentRef.current}</Text> </TestCase>
-          <TestCase itShould="渲染子页面页脚 renderFeet"> <Text>{renderFeetRef.current}</Text> </TestCase>          
-          <TestCase itShould="渲染子页面标题 renderSectionTitle"> <Text>{renderSectionTitleRef.current}</Text> </TestCase>
+          <TestCase itShould="点击展开页面标题回调 onChange"><Text>{renderHeaderRef.current}</Text></TestCase>
+          <TestCase itShould="渲染子页面头 renderHeader"><Text>{onChangeRef.current}</Text></TestCase>
+          <TestCase itShould="渲染子页面内容 renderContent"><Text>{renderContentRef.current}</Text></TestCase>
+          <TestCase itShould="渲染子页面页脚 renderFeet"><Text>{renderFeetRef.current}</Text></TestCase>          
+          <TestCase itShould="渲染子页面标题 renderSectionTitle"><Text>{renderSectionTitleRef.current}</Text></TestCase>
         </TestSuite>
       </Tester>
     </ScrollView>
@@ -239,19 +263,19 @@ const BACON_IPSUM1 =
   '11Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs. Picanha beef prosciutto meatball turkey shoulder shank salami cupim doner jowl pork belly cow. Chicken shankle rump swine tail frankfurter meatloaf ground round flank ham hock tongue shank andouille boudin brisket. ';
 
 const CONTENT = [
-  {title: 'First', feet: '1', content: "1111",SectionTitle:'1st'},
-  {title: 'Second', feet: '2', content: "2222",SectionTitle:'2st'},
-  {title: 'Third', feet: '3', content: "3333",SectionTitle:'3st'},
-  {title: 'Fourth', feet: '4', content: "44444",SectionTitle:'4st'},
-  {title: 'Fifth', feet: '5', content: "55555",SectionTitle:'5st'},
+  {title: 'First', feet: '1', content: "1111",SectionTitle:'1st',ekey:2},
+  {title: 'Second', feet: '2', content: "2222",SectionTitle:'2st',ekey:3},
+  {title: 'Third', feet: '3', content: "3333",SectionTitle:'3st',ekey:4},
+  {title: 'Fourth', feet: '4', content: "44444",SectionTitle:'4st',ekey:5},
+  {title: 'Fifth', feet: '5', content: "55555",SectionTitle:'5st',ekey:1},
 ];
 
 const CONTENT1 = [
-  {title: 'First1', feet: '1', content: "1111z"},
-  {title: 'Second1', feet: '2', content: "22222z"},
-  {title: 'Third1', feet: '3', content: "333333z"},
-  {title: 'Fourth1', feet: '4', content: "4444z"},
-  {title: 'Fifth1', feet: '5', content: "5555555z"},
+  {title: 'First1', feet: '1', content: "1111z",ekey:2},
+  {title: 'Second1', feet: '2', content: "22222z",ekey:3},
+  {title: 'Third1', feet: '3', content: "333333z",ekey:4},
+  {title: 'Fourth1', feet: '4', content: "4444z",ekey:5},
+  {title: 'Fifth1', feet: '5', content: "5555555z",ekey:1},
 ];
 
 const SELECTORS = [
