a449021753fa722736fdc616198f576304b09dd9
ding_chengjie <dingchenjie1@h-partners.com>
2024-07-25 19:23:30 +0800

    test:修改masked-view demo
    Signed-off-by: ding_chengjie <dingchenjie1@h-partners.com>

--- @react-native-masked-view/masked-view/MaskedDemo.tsx
+++ @react-native-masked-view/masked-view/MaskedDemo.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
-
+import { View, StyleSheet, Text, Image } from 'react-native';
 import MaskedView from '@react-native-masked-view/masked-view';
-
-const Colors = {
-    americanBlue: '#324376',
-    bittersweet: '#F76C5E',
-    black: 'black',
-    chineseWhite: '#E1E1E1',
-    darkChestnut: '#96645D',
-    khaki: '#F5DD90',
-};
 
 const MaskedDemo = () => {
     return (
@@ -21,15 +11,16 @@ const MaskedDemo = () => {
                     <Text style={styles.maskElementText}>Basic Mask</Text>
                     <Text style={styles.maskElementText}>Basic Mask</Text>
                     <Text style={styles.maskElementText}>Basic Mask</Text>
-                    <Text style={styles.maskElementText}>Basic Mask</Text>
-                    <Text style={styles.maskElementText}>Basic Mask</Text>
-                 </View>
+                </View>
             }
         >
-        <Text style={styles.textView}>Basic Mask666 888 123</Text>
-            <View style={{ width:'100%', height: 40, backgroundColor: '#fe4b83' }} />
-            <View style={{ width:'100%', height: 40, backgroundColor: '#F5DD90' }} />
-             <Image
+            <View style={{ flex: 1, flexDirection: 'row' }}>
+                <View style={{ width: '25%', height: 80, backgroundColor: Colors.americanBlue }} />
+                <View style={{ width: '25%', height: 80, backgroundColor: Colors.khaki }} />
+                <View style={{ width: '25%', height: 80, backgroundColor: Colors.bittersweet }} />
+                <View style={{ width: '25%', height: 80, backgroundColor: Colors.chineseWhite }} />
+            </View>
+            <Image
                 resizeMode="cover"
                 source={{ uri: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F6651224a-b18d-4456-97b8-14c6daad236d%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1694154457&t=37e024c7f8fb33d10e154f32353baf4a' }}
                 style={styles.image}
@@ -47,50 +38,59 @@ const MaskedDemo = () => {
                     education, bigger civic improvements, and lower taxes. On election day,
                     cast your vote for a proven leader, re-elect Mayor Red Thomas...
                 </Text>
-            </View> 
+            </View>
         </MaskedView>
-    )
-}
+    );
+};
+
+const Colors = {
+    americanBlue: '#324376',
+    bittersweet: '#F76C5E',
+    black: 'black',
+    chineseWhite: '#E1E1E1',
+    darkChestnut: '#96645D',
+    khaki: '#F5DD90',
+};
 
 const styles = StyleSheet.create({
     maskedView: {
-        width:'100%',
-        height:'40%'
+        width: '100%',
+        height: '250'
     },
     maskElementView: {
         backgroundColor: 'transparent',
         alignItems: 'center',
-        width:'100%',
-        height:'100%',
+        width: '100%',
+        height: '100%',
         justifyContent: 'center',
     },
     maskElementText: {
         color: Colors.black,
-        width:'100%',
-        height:60,
+        width: '100%',
+        height: 80,
         fontSize: 50,
         fontWeight: 'bold',
     },
     image: {
-        width:'100%',
-        height:140,
+        width: '100%',
+        height: 80,
     },
     textView: {
         fontSize: 20,
         alignItems: 'center',
-        width:'100%',
-        height:40,
+        width: '100%',
+        height: 80,
         flexDirection: 'row',
         justifyContent: 'center',
     },
     text: {
         color: Colors.darkChestnut,
         fontSize: 8,
-        width:'100%',
-        height:40,
+        width: '100%',
+        height: 80,
         fontVariant: ['small-caps'],
         fontWeight: 'bold',
     },
 });
 
-export default MaskedDemo
+export default MaskedDemo;
