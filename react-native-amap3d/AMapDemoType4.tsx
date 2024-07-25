a51f8a3da0f0d1bff2ad843aeaac5a1d74ae5d47
17382561546 <liangzhenhua1@h-partners.com>
2024-07-25 15:58:51 +0800

    test: 修改Demo属性对比方式，事件响应方式，删除XTS不支持属性

--- react-native-amap3d/AMapDemoType4.tsx
+++ react-native-amap3d/AMapDemoType4.tsx
@@ -43,13 +43,11 @@ function AMapDemo() {
   return (
     <View style= { styles.container } >
       <MapView 
-        mapType={ 2 }
-        tiltGesturesEnabled = { true}
-        rotateGesturesEnabled = { true}
-        scrollGesturesEnabled = { true}
-        zoomGesturesEnabled = { true}
-        minZoom = { 5}
-        maxZoom = { 15}
+        mapType={ 3 }
+        tiltGesturesEnabled = { false}
+        rotateGesturesEnabled = { false}
+        scrollGesturesEnabled = { false}
+        zoomGesturesEnabled = { false}
         trafficEnabled = { false}
         labelsEnabled = { false}
         buildingsEnabled = { false}
@@ -60,33 +58,10 @@ function AMapDemo() {
             latitude: 38.91095,
             longitude: 115.37296,
           },
-          zoom: 7,
+          zoom: 11,
         }
       }
       > 
-      <Circle
-        strokeWidth={ 10 }
-        strokeColor = "rgba(0, 255, 0, 0.5)"
-        fillColor = "rgba(255, 0, 0, 0.5)"
-        radius = { 2000}
-        center = {{ latitude: 39.906901, longitude: 116.397972 }}
-        zIndex = { 1}
-      />
-      <Polygon
-        strokeWidth={ 10 }
-        strokeColor = "rgba(95, 36, 51, 0.5)"
-        fillColor = "rgba(255, 15, 123, 0.5)"
-        zIndex = { 1}
-        points = { points2 }
-      />
-      <Polyline width={ 200 } color = "rgba(0, 255, 0, 0.5)" points = { line1 } dotted = {true}  />
-    
-      <Marker
-        draggable={ false }
-        flat = { false}
-        zIndex = { 1}
-        position = {{ latitude: 39.856901, longitude: 116.407972 }}
-      />
       </MapView>
     </View>
 );
