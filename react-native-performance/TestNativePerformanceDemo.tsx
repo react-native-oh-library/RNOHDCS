693cc64f3d5185ec09e09500cce460fe5d8b48fc
jinzhao <wangjingyao4@h-partners.com>
2024-07-29 20:08:34 +0800

    test:修改示例demo中react-native-performance库的引入方式

--- react-native-performance/TestNativePerformanceDemo.tsx
+++ react-native-performance/TestNativePerformanceDemo.tsx
@@ -21,7 +21,7 @@ import {
 import performance, {
   PerformanceObserver,
   setResourceLoggingEnabled,
-} from '@react-native-oh-tpl/react-native-performance';
+} from 'react-native-performance';
 const PALETTE = {
   REACT_CYAN_LIGHT: 'hsl(193, 95%, 68%)',
   REACT_CYAN_DARK: 'hsl(193, 95%, 30%)',
