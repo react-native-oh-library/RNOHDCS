67cc143a26701f1b56341af83502ed6a6c9ab398
yaoZZYff <1228091089@qq.com>
2024-07-29 20:28:50 +0800

    add react-native-keyboard-accessory test (#656)
    Co-authored-by: ZhuYao <n035824@archermind.com>

---
+++ react-native-keyboard-accessory/KeyboardAwareTabBar.jsx
@@ -0,0 +1,45 @@
+// App.js
+import React from 'react'
+import { View, Text, Keyboard, Platform, SafeAreaView, StyleSheet, TextInput} from 'react-native';
+import { KeyboardAwareTabBarComponent } from 'react-native-keyboard-accessory';
+
+
+
+export default function KeyboardAwareTabBarTest() {
+  const [textInputValue, setTextInputValue] = React.useState('');
+  const TabBar = () => {
+  return (
+    <View style={{ backgroundColor: 'lightblue', padding: 10 }}>
+      <Text>This is a TabBar</Text>
+    </View>
+  );
+};
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        value={textInputValue}
+        onChangeText={setTextInputValue}
+        placeholder="Type here..."
+      />
+      <KeyboardAwareTabBarComponent TabBarComponent={TabBar} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'top',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  textInput: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 20,
+    paddingHorizontal: 10,
+  },
+});
