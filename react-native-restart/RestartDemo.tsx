9c0afd8918f4ca56b64d35b24927c5b32033abbb
hewei <hewei203@h-partners.com>
2024-07-30 17:29:12 +0800

    test: 修改react-native-restart测试demo

--- react-native-restart/RestartDemo.tsx
+++ react-native-restart/RestartDemo.tsx
@@ -1,20 +1,63 @@
-import {Button, Text, View, Alert} from 'react-native';
-import RNRestart from 'react-native-restart';
-import {Tester, TestSuite} from '@rnoh/testerino';
-import {TestCase} from '../../components/TestCase';
+import { useState } from "react";
+import { StyleSheet, Text } from "react-native";
+import RNRestart from "react-native-restart";
+import { Tester, TestSuite, TestCase } from "@rnoh/testerino";
 
+export default function RestartDemo() {
+  const [restartReason, setRestartReason] = useState("");
 
-export default function RestartDemoTester() {
   return (
     <Tester>
       <TestSuite name="restart">
-        <TestCase.Example tags={['C_API']} itShould="restart">
-          <Button title="重启(restart)" onPress={() => RNRestart.restart()} />
-        </TestCase.Example>
-        <TestCase.Example tags={['C_API']} itShould="Restart">
-          <Button title="重启(Restart)" onPress={() => RNRestart.Restart()} />
-        </TestCase.Example>
+        <TestCase tags={["C_API"]} itShould="restart">
+          <Text
+            style={styles.button}
+            onPress={() => RNRestart.restart("restart_a")}
+          >
+            restart
+          </Text>
+        </TestCase>
+        <TestCase tags={["C_API"]} itShould="Restart">
+          <Text
+            style={styles.button}
+            onPress={() => RNRestart.Restart("restart_b")}
+          >
+            Restart
+          </Text>
+        </TestCase>
+        <TestCase
+          tags={["C_API"]}
+          itShould="getReason"
+          initialState={"reason"}
+          arrange={({ setState }) => {
+            return (
+              <Text
+                style={styles.button}
+                onPress={async () => {
+                  const reason = await RNRestart.getReason();
+                  setRestartReason(reason);
+                  setState(reason);
+                }}
+              >
+                getReason{restartReason ? `（原因：${restartReason}）` : ""}
+              </Text>
+            );
+          }}
+          assert={async ({ expect, state }) => {
+            expect(state).include("restart_");
+          }}
+        />
       </TestSuite>
     </Tester>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    backgroundColor: "hsl(193, 95%, 68%)",
+    borderWidth: 2,
+    borderColor: "hsl(193, 95%, 30%)",
+  },
+});
