3e20b50a45472e978ccc55399a3f59089de625d2
misaka10032 <78400494+ding-chengjie@users.noreply.github.com>
2024-07-31 19:14:28 +0800

    test:修改react-native-switch-pro的test (#687)

--- react-native-switch-pro/test/switchProTest.tsx
+++ react-native-switch-pro/test/switchProTest.tsx
@@ -1,6 +1,6 @@
 import { TestSuite, TestCase, Tester } from '@rnoh/testerino';
 import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, ScrollView } from 'react-native';
 import Switch from 'react-native-switch-pro';
 
 export default class SwitchProTest extends Component {
@@ -12,34 +12,61 @@ export default class SwitchProTest extends Component {
         return (
             <Tester>
                 <TestSuite name="SwitchPro">
-                    <TestCase itShould="SwitchPro View">
-                        <View style={styles.container}>
-                            <Switch circleColorActive='white' circleColorInactive='white' />
-                            <Text>uncontrolled</Text>
-                            <Switch
-                                label={2}
-                                value={this.state.value}
-                                style={{ marginTop: 20 }}
-                                onSyncPress={value => this.setState({ value })}
-                            />
-                            <Text> two way binding</Text>
-                            <Switch
-                                label={3}
-                                value={this.state.value}
-                                style={{ marginTop: 20 }}
-                            />
-                            <Text> controlled by outside</Text>
-                            <Switch
-                                width={60}
-                                height={30}
-                                style={{ marginTop: 20 }}
-                                value={this.state.value}
-                                onAsyncPress={callback => {
-                                    setTimeout(() => callback(true), 1000);
-                                }}
-                            />
-                        </View>
-                    </TestCase>
+                    <ScrollView>
+                        <TestCase itShould="style、label、value、onAsyncPress、onSyncPress">
+                            <View style={styles.container}>
+                                <Switch
+                                    label={2}
+                                    value={this.state.value}
+                                    style={{ marginTop: 20 }}
+                                    onSyncPress={value => this.setState({ value })}
+                                />
+                                <Text> two way binding</Text>
+                                <Switch
+                                    label={3}
+                                    value={this.state.value}
+                                    style={{ marginTop: 20 }}
+                                />
+                                <Text> controlled by outside</Text>
+                                <Switch
+                                    style={{ marginTop: 20 }}
+                                    value={this.state.value}
+                                    onAsyncPress={callback => {
+                                        setTimeout(() => callback(true), 1000);
+                                    }}
+                                />
+                            </View>
+                        </TestCase>
+                        <TestCase itShould="width、height">
+                            <View style={styles.container}>
+                                <Switch width={60} height={30} />
+                            </View>
+                        </TestCase>
+                        <TestCase itShould="circleColorActive、circleColorInactive">
+                            <View style={styles.container}>
+                                <Switch circleColorActive="black" circleColorInactive="red" />
+                            </View>
+                        </TestCase>
+                        <TestCase itShould="disabled">
+                            <View style={styles.container}>
+                                <Switch disabled={false} />
+                                <Switch style={{ marginTop: 20 }} disabled={true} />
+                            </View>
+                        </TestCase>
+                        <TestCase itShould="circleStyle">
+                            <View style={styles.container}>
+                                <Switch circleStyle={{ backgroundColor: '#230dff' }} />
+                            </View>
+                        </TestCase>
+                        <TestCase itShould="backgroundInactive、backgroundActive">
+                            <View style={styles.container}>
+                                <Switch
+                                    backgroundInactive="#0061f782"
+                                    backgroundActive="#1cfdff82"
+                                />
+                            </View>
+                        </TestCase>
+                    </ScrollView>
                 </TestSuite>
             </Tester>
         );
@@ -48,11 +75,11 @@ export default class SwitchProTest extends Component {
 
 const styles = StyleSheet.create({
     container: {
+        height: 300,
+        width: 350,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#5174a2',
-        height: 500,
-        width: 350,
     },
     welcome: {
         fontSize: 20,
