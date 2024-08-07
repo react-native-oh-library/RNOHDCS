47e21a9420fe1b752fa3b694d61298ab07f519a8
ddwwwww <18829031738@163.com>
2024-08-07 17:43:55 +0800

    test: 修改react-native-base64测试用例

--- react-native-base64/Test/Base64Test.tsx
+++ react-native-base64/Test/Base64Test.tsx
@@ -4,53 +4,33 @@ import {
   ScrollView,
   Text,
   View,
-  Button
+  Button,
+  TextInput,
+  StyleSheet
 } from 'react-native';
 import { Tester, TestCase } from '@rnoh/testerino';
 
-export default function Base64Test() {
+export function Base64Test() {
   const [word, setWord] = useState('react native');
-  const [flag, setFlag] = useState(false);
+  const [encodeWord, setEncodeWord] = useState('');
+  const [byteArrayWord, setByteArrayWord] = useState('');
   const [decodeWord, setdecodeWord] = useState('');
-  const [flag1, setFlag1] = useState(false);
   const [Unit8ArrayWord, setUnit8ArrayWord] = useState('');
   const [decodeUnit8ArrayWord, setdecodeUnit8ArrayWord] = useState('');
-
-  const AddBase64Encode = (props: {
-    setState: React.Dispatch<React.SetStateAction<string>>;
-  }) => {
-    const encrypt = () => {
-      if(!flag){
-        const encodeWord = base64.encode(word);
-        setWord(encodeWord);
-        props.setState(encodeWord);
-        setFlag(true);
-      }
-    };
-
-    return (
-      <View style={{ height: 50 }}>
-        <Text>{word}</Text>
-        <Button title="编码" onPress={encrypt} />
-      </View>
-    );
-  };
+  const byte = Uint8Array.from(word.split(''), w => w.charCodeAt(0));
 
   const AddBase64Decode = (props: {
     setState: React.Dispatch<React.SetStateAction<string>>;
   }) => {
     const decrypt = () => {
-      if(!flag1){
-        const decodeWord = base64.decode(word);
-        setdecodeWord(decodeWord);
-        props.setState(decodeWord);
-        setFlag1(true);
-      }
+      const decodeWord = base64.decode(encodeWord);
+      setdecodeWord(decodeWord);
+      props.setState(decodeWord);
     };
 
     return (
       <View style={{ height: 50 }}>
-        <Text>{decodeWord}</Text>
+        <Text>解码值：{decodeWord}</Text>
         <Button title="解码" onPress={decrypt} />
       </View>
     );
@@ -59,18 +39,17 @@ export default function Base64Test() {
   const AddBase64EncodeUnit8Array = (props: {
     setState: React.Dispatch<React.SetStateAction<string>>;
   }) => {
-    const testWord = 'react native';
-    const byteArrayWord = Uint8Array.from(testWord.split(''), w => w.charCodeAt(0));
+    setByteArrayWord(byte.toString());
     const encrypt = () => {
-      const encodeWord = base64.encodeFromByteArray(byteArrayWord);
+      const encodeWord = base64.encodeFromByteArray(Uint8Array.from(word.split(''), w => w.charCodeAt(0)));
       setUnit8ArrayWord(encodeWord);
       props.setState(encodeWord);
     };
 
     return (
-      <View style={{ height: 70 }}>
-        <Text>{byteArrayWord}</Text>
-        <Text>{Unit8ArrayWord}</Text>
+      <View style={{ height: 90 }}>
+        <Text style={{ borderWidth: 1 }}>Unit8Array字符：{byteArrayWord}</Text>
+        <Text>编码值：{Unit8ArrayWord}</Text>
         <Button title="Unit8Array编码" onPress={encrypt} />
       </View>
     );
@@ -79,7 +58,7 @@ export default function Base64Test() {
   const AddBase64DecodeUnit8Array = (props: {
     setState: React.Dispatch<React.SetStateAction<string>>;
   }) => {
-    const encrypt = () => {
+    const decrypt = () => {
       const decodeWord = base64.decode(Unit8ArrayWord);
       setdecodeUnit8ArrayWord(decodeWord);
       props.setState(decodeWord);
@@ -87,8 +66,8 @@ export default function Base64Test() {
 
     return (
       <View style={{ height: 50 }}>
-        <Text>{decodeUnit8ArrayWord}</Text>
-        <Button title="Unit8Array解码" onPress={encrypt} />
+        <Text>解码值：{decodeUnit8ArrayWord}</Text>
+        <Button title="Unit8Array解码" onPress={decrypt} />
       </View>
     );
   };
@@ -100,7 +79,28 @@ export default function Base64Test() {
           <TestCase
             itShould="Test encoding word."
             initialState={''}
-            arrange={({ setState }) => <AddBase64Encode setState={setState} />}
+            arrange={({ setState }) => {
+              const encrypt = () => {
+                const encodeWord = base64.encode(word);
+                setEncodeWord(encodeWord);
+                setState(encodeWord);
+              };
+
+              return (
+                <View style={{ height: 100 }}>
+                  <TextInput
+                    key="textinput2"
+                    style={styles.TextInput}
+                    onChangeText={(newValue) => {
+                      setWord(newValue);
+                    }}
+                    value={word}
+                  ></TextInput>
+                  <Text>编码值：{encodeWord}</Text>
+                  <Button title="编码" onPress={encrypt} />
+                </View>
+              );
+            }}
             assert={({ expect, state }) => {
               expect(state).to.be.string;
             }}
@@ -110,7 +110,7 @@ export default function Base64Test() {
             initialState={''}
             arrange={({ setState }) => <AddBase64Decode setState={setState} />}
             assert={({ expect, state }) => {
-              expect(state).equal('react native');
+              expect(state).equal(word);
             }}
           />
           <TestCase
@@ -126,7 +126,7 @@ export default function Base64Test() {
             initialState={''}
             arrange={({ setState }) => <AddBase64DecodeUnit8Array setState={setState} />}
             assert={({ expect, state }) => {
-              expect(state).equal('react native');
+              expect(state).equal(word);
             }}
           />
         </Tester>
@@ -134,3 +134,7 @@ export default function Base64Test() {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  TextInput: { height: 40, borderColor: '#ccc', borderWidth: 1, borderRadius: 4, width: '100%' },
+});
