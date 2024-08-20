812ca0d62cb00a8e58ebca9bb3549868a6808b24
mingmingaini <43883991+mingmingaini@users.noreply.github.com>
2024-08-20 16:47:14 +0800

    test:修改react-native-fs测试用例 (#907)

--- react-native-fs/tests/FsTest.tsx
+++ react-native-fs/tests/FsTest.tsx
@@ -17,6 +17,8 @@ function FsTest(): React.JSX.Element {
     // 参数
     const [mkdirParam, setMkdirParam] = useState("");
     const [existsParams, setExistsParams] = useState("");
+    const [readDirExampleData, setreadDirExampleData] = useState({})
+    const [writeFileExampleData, setwriteFileExampleData] = useState('')
     const mkdirExample = () => {
         //创建文件夹
         RNFS.mkdir(RNFS.DocumentDirectoryPath + "/" + mkdirParam).then(
@@ -46,12 +48,13 @@ function FsTest(): React.JSX.Element {
         //读取文件夹
         RNFS.readDir(RNFS.DocumentDirectoryPath) // On Android, use "RNFS.DocumentDirectoryPath" (MainBundlePath is not defined)
             .then((result) => {
-    
+
                 console.log('readDirExample', result);
-            
-            }).catch(err=>{
-                console.log('readDirExamplereadDirExample',err);
-                
+                setreadDirExampleData(result)
+
+            }).catch(err => {
+                console.log('readDirExamplereadDirExample', err);
+
             })
     };
 
@@ -66,6 +69,7 @@ function FsTest(): React.JSX.Element {
         RNFS.writeFile(path, 'Lorem ipsum dolor sit amet', 'utf8')//写入文件
             .then((success) => {
                 console.log('writeFileExample' + RNFS.DocumentDirectoryPath + '/2.txt', success);
+                setwriteFileExampleData(path)
             })
             .catch((err) => {
                 console.log(err.message);
@@ -89,15 +93,16 @@ function FsTest(): React.JSX.Element {
 
 
     const [readFileParam, setReadFileParam] = useState("");
-
+    const [readFileExampleData, setreadFileExampleData] = useState({});
     const readFileExample = () => {
         RNFS.readFile(RNFS.DocumentDirectoryPath + `/${readFileParam}`)
-        .then((contents) => {
-            console.log("readFileExample",contents.length);
-        })
-        .catch((err) => {
-            console.log('ygb 读取失败：',err.message, err.code);
-        });
+            .then((contents) => {
+                console.log("readFileExample", contents.length);
+                setreadFileExampleData(contents)
+            })
+            .catch((err) => {
+                console.log('ygb 读取失败：', err.message, err.code);
+            });
     }
 
 
@@ -115,8 +120,8 @@ function FsTest(): React.JSX.Element {
 
     const copyFileExample = () => {
 
-        const path = RNFS.DocumentDirectoryPath + '/test.txt';
-        const path1 = RNFS.DocumentDirectoryPath + '/eee/4.txt';
+        const path = RNFS.DocumentDirectoryPath + '/2.txt';
+        const path1 = RNFS.DocumentDirectoryPath + '/3.txt';
         RNFS.copyFile(path, path1)
             .then((result) => {
                 console.log("copyFileExample", path, path1);
@@ -143,25 +148,26 @@ function FsTest(): React.JSX.Element {
 
 
 
-
+    const [readExampleData,setreadExampleData]=useState({})
     const readExample = () => {
-        const path = RNFS.DocumentDirectoryPath + '/eee/3.txt';
-        RNFS.read(path)
+        const path = RNFS.DocumentDirectoryPath + '/2.txt';
+        RNFS.read(path, 1024, 0, 'utf8')
             .then((result) => {
                 console.log("readExample", result);
+                setreadExampleData(result)
             })
             .catch((err) => {
                 console.log(err.message);
             });
     }
 
-
-
+    const [starExampleData,setstarExampleData]=useState({})
     const starExample = () => {
         const path = RNFS.DocumentDirectoryPath + '/eee/3.txt';
         RNFS.stat(path)
             .then((result) => {
                 console.log("starExample", result);
+                setstarExampleData(result)
             })
             .catch((err) => {
                 console.log(err.message);
@@ -171,24 +177,24 @@ function FsTest(): React.JSX.Element {
     const downloadFileExample = () => {
         RNFS.downloadFile({
             fromUrl: "https://www-file.huawei.com/minisite/media/annual_report/annual_report_2022_cn.pdf",// URL to download file from
-            toFile: RNFS.DocumentDirectoryPath + '/eee/3.pdf'
+            toFile: RNFS.DocumentDirectoryPath + '/3.pdf'
         })
     }
 
     const readFileAssetsExample = () => {//用户获取resources/rawfile/1.txt 目录下对应的rawfile文件内容，使用callback形式返回字节数组。
-        RNFS.readFileAssets('1.txt').then(res => {
+        RNFS.readFileAssets('1.txt','utf8').then(res => {
 
-            console.log("downloadFileExample", res);
+            console.log("readFileAssetsExample", res);
 
         }).catch((err) => {
-            console.log("downloadFileExample error", err);
+            console.log("readFileAssetsExample error", err);
         });
     }
 
 
 
-    const existsAssetsExample = () => {//用户获取resources/rawfile/1.txt 目录下对应的rawfile文件内容，使用callback形式返回字节数组。
-        RNFS.existsAssets("test").then(res => {
+    const existsAssetsExample = () => {//用户获取resources/rawfile/ 底下有没有assets
+        RNFS.existsAssets("assets").then(res => {
 
             console.log("existsAssetsExample", res);
 
@@ -216,9 +222,9 @@ function FsTest(): React.JSX.Element {
                             initialState={false}
                             arrange={({ setState }) => {
                                 return (
-                                    <View style={{  padding: 20 }}>
+                                    <View style={{ padding: 20 }}>
                                         <View>
-                                            <Text style={{fontSize:13,color:"red"}}>
+                                            <Text style={{ fontSize: 13, color: "red" }}>
                                                 当前库为文件操作库 不存在页面效果请关注文件系统变更{"\n"}
                                                 相关资料链接：https://developer.huawei.com/consumer/cn/doc/harmonyos-guides/app-sandbox-directory-0000001774280086
                                             </Text>
@@ -252,7 +258,7 @@ function FsTest(): React.JSX.Element {
                             initialState={false}
                             arrange={({ setState }) => {
                                 return (
-                                    <View style={{ height: 100, padding: 20 }}>
+                                    <View style={{ height: 200, padding: 20 }}>
                                         <View style={styles.sectionDescription}>
                                             <TextInput
                                                 style={styles.input}
@@ -262,6 +268,7 @@ function FsTest(): React.JSX.Element {
                                                 autoCapitalize="none"
                                             />
                                         </View>
+                                        <View>     <Text>{RNFS.DocumentDirectoryPath + "/" + mkdirParam}</Text></View>
                                         <Button
                                             title="Create Directory"
                                             color="#9a73ef"
@@ -282,13 +289,16 @@ function FsTest(): React.JSX.Element {
                             initialState={false}
                             arrange={({ setState }) => {
                                 return (
-                                    <View style={{ height: 100, padding: 20 }}>
-                                        <Button
-                                            title="readDirExample"
-                                            color="#9a73ef"
-                                            onPress={() => { readDirExample(); setState(true) }}
-                                        />
-                                    </View>
+                                    <ScrollView style={{ height: 200 }}>
+                                        <View style={{ height: 100, padding: 20 }}>
+                                            <Text>{JSON.stringify(readDirExampleData)}</Text>
+                                            <Button
+                                                title="readDirExample"
+                                                color="#9a73ef"
+                                                onPress={() => { readDirExample(); setState(true) }}
+                                            />
+                                        </View>
+                                    </ScrollView>
                                 );
                             }}
                             assert={async ({ expect, state }) => {
@@ -305,6 +315,7 @@ function FsTest(): React.JSX.Element {
                             arrange={({ setState }) => {
                                 return (
                                     <View style={{ height: 100, padding: 20 }}>
+                                        <Text>{writeFileExampleData}</Text>
                                         <Button
                                             title="writeFileExample"
                                             color="#9a73ef"
@@ -326,6 +337,7 @@ function FsTest(): React.JSX.Element {
                             arrange={({ setState }) => {
                                 return (
                                     <View style={{ height: 100, padding: 20 }}>
+                                        <Text>{RNFS.DocumentDirectoryPath + '/2.txt'}</Text>
                                         <Button
                                             title="unlinkExample"
                                             color="#9a73ef"
@@ -345,15 +357,18 @@ function FsTest(): React.JSX.Element {
                             initialState={false}
                             arrange={({ setState }) => {
                                 return (
-                                    <View style={{ height: 100, padding: 20 }}>
+                                    <View style={{ height: 130, padding: 20 }}>
+                                        <Text>{RNFS.DocumentDirectoryPath + `/${readFileParam}`}</Text>
+                                        <Text>{JSON.stringify(readFileExampleData)}</Text>
                                         <View>
-                                        <TextInput
+                                            <TextInput
                                                 style={styles.input}
                                                 placeholder="Folder Path"
                                                 onChangeText={(ReadFileParam) => setReadFileParam(ReadFileParam)}
                                                 placeholderTextColor="#9a73ef"
                                                 autoCapitalize="none"
                                             />
+
                                         </View>
                                         <Button
                                             title="readFileExample"
@@ -376,6 +391,7 @@ function FsTest(): React.JSX.Element {
                             arrange={({ setState }) => {
                                 return (
                                     <View style={{ height: 100, padding: 20 }}>
+                                        <Text>{RNFS.DocumentDirectoryPath + '/2.txt'}</Text>
                                         <Button
                                             title="appendFileExample"
                                             color="#9a73ef"
@@ -427,7 +443,10 @@ function FsTest(): React.JSX.Element {
                             initialState={false}
                             arrange={({ setState }) => {
                                 return (
-                                    <View style={{ height: 100, padding: 20 }}>
+                                    <View style={{ height: 140, padding: 20 }}>
+                                        <Text>{RNFS.DocumentDirectoryPath + '/2.txt'} </Text>
+                                        
+                                        <Text>{RNFS.DocumentDirectoryPath + '/3.txt'}</Text>
                                         <Button
                                             title="copyFileExample"
                                             color="#9a73ef"
@@ -451,7 +470,10 @@ function FsTest(): React.JSX.Element {
                             initialState={false}
                             arrange={({ setState }) => {
                                 return (
-                                    <View style={{ height: 100, padding: 20 }}>
+                                    <View style={{ height: 160, padding: 20 }}>
+                                        <Text>{ RNFS.DocumentDirectoryPath + '/3.txt'}</Text>
+                                        <Text>{RNFS.DocumentDirectoryPath + '/eee/3.txt'}</Text>
+                                        <Text>先用第一个create dir创建一个eee目录</Text>
                                         <Button
                                             title="moveFileExample"
                                             color="#9a73ef"
@@ -475,6 +497,7 @@ function FsTest(): React.JSX.Element {
                             arrange={({ setState }) => {
                                 return (
                                     <View style={{ height: 100, padding: 20 }}>
+                                        <Text>{JSON.stringify(readExampleData)}</Text>
                                         <Button
                                             title="readExample"
                                             color="#9a73ef"
@@ -499,6 +522,7 @@ function FsTest(): React.JSX.Element {
                             arrange={({ setState }) => {
                                 return (
                                     <View style={{ height: 100, padding: 20 }}>
+                                        <Text>{JSON.stringify(starExampleData)}</Text>
                                         <Button
                                             title="starExample"
                                             color="#9a73ef"
@@ -513,26 +537,7 @@ function FsTest(): React.JSX.Element {
                         />
 
 
-                        <TestCase
-                            key={"starExample"}
-                            itShould="fs api downloadFileExample"
-                            tags={['C_API']}
-                            initialState={false}
-                            arrange={({ setState }) => {
-                                return (
-                                    <View style={{ height: 100, padding: 20 }}>
-                                        <Button
-                                            title="downloadFileExample"
-                                            color="#9a73ef"
-                                            onPress={() => { downloadFileExample(); setState(true) }}
-                                        />
-                                    </View>
-                                );
-                            }}
-                            assert={async ({ expect, state }) => {
-                                expect(state).to.be.true;
-                            }}
-                        />
+            
 
 
                         <TestCase
@@ -543,6 +548,10 @@ function FsTest(): React.JSX.Element {
                             arrange={({ setState }) => {
                                 return (
                                     <View style={{ height: 100, padding: 20 }}>
+                                        <Text>{ RNFS.DocumentDirectoryPath + '/3.pdf'}</Text>
+                                        <Text>
+                                            
+                                        </Text>
                                         <Button
                                             title="downloadFileExample"
                                             color="#9a73ef"
@@ -564,6 +573,7 @@ function FsTest(): React.JSX.Element {
                             arrange={({ setState }) => {
                                 return (
                                     <View style={{ height: 100, padding: 20 }}>
+                                        <Text>resources/rawfile/1.txt </Text>
                                         <Button
                                             title="readFileAssetsExample"
                                             color="#9a73ef"
@@ -586,6 +596,7 @@ function FsTest(): React.JSX.Element {
                             arrange={({ setState }) => {
                                 return (
                                     <View style={{ height: 100, padding: 20 }}>
+                                        <Text>//用户获取resources/rawfile/ 底下有没有assets</Text>
                                         <Button
                                             title="existsAssetsExample"
                                             color="#9a73ef"
