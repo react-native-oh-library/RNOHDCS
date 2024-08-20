09f11e91da9790ed850f7a04f7b99aeb70fc6754
kongchuiyu <kongchuiyu@h-partners.com>
2024-08-20 11:48:09 +0800

    test：修改react-native-fast-image测试用例

--- react-native-fast-image/tester/FastImageSourceDemo.tsx
+++ react-native-fast-image/tester/FastImageSourceDemo.tsx
@@ -1,32 +1,32 @@
-import React, {} from 'react';
-import { StyleSheet, ScrollView} from 'react-native';
+import React from 'react';
+import {StyleSheet, ScrollView} from 'react-native';
 import FastImage from 'react-native-fast-image';
 import {useCacheBust} from './utils/useCacheBust';
 import {TestSuite, Tester, TestCase} from '@rnoh/testerino';
 
 const IMAGE_URLS = [
-  "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEcdM.img",
-  "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEcgc.img",
-  "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAOEhXT.img",
-  "https://i.loli.net/2021/04/14/nNly8EdXJ2aHYTe.jpg",
-  "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
-  "https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg"
+  'http://fpoimg.com/100x100',
+  'http://fpoimg.com/200x200',
+  'http://fpoimg.com/400x400',
+  'http://fpoimg.com/100x300',
+  'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+  'https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg',
 ];
 
-export const FastImagePriorityCacheDemo = () => {
+export const FastImageSourceDemo = () => {
   const {query, bust} = useCacheBust('');
   return (
     <Tester>
       <ScrollView>
         <TestSuite name="priority && cache">
           <TestCase itShould="priority low">
-              <FastImage
-                style={styles.image}
-                source={{
-                  uri: IMAGE_URLS[0],
-                  priority: FastImage.priority.low,
-                }}
-              />
+            <FastImage
+              style={styles.image}
+              source={{
+                uri: IMAGE_URLS[0],
+                priority: FastImage.priority.low,
+              }}
+            />
           </TestCase>
 
           <TestCase itShould="priority normal">
@@ -72,6 +72,47 @@ export const FastImagePriorityCacheDemo = () => {
               source={{
                 uri: IMAGE_URLS[5],
                 cache: FastImage.cacheControl.cacheOnly,
+              }}
+            />
+          </TestCase>
+        </TestSuite>
+        <TestSuite name="uri">
+          <TestCase itShould="image 1">
+            <FastImage
+              style={styles.image}
+              source={{
+                uri: "https://i-blog.csdnimg.cn/blog_migrate/12ea5d8f226ea294f76a8e77cfaf24c5.jpeg",
+              }}
+            />
+          </TestCase>
+          <TestCase itShould="image 2">
+            <FastImage
+              style={styles.image}
+              source={{
+                uri: "https://i-blog.csdnimg.cn/blog_migrate/f7cd0c9c94a756902bc24ae4d115f13a.jpeg",
+              }}
+            />
+          </TestCase>
+        </TestSuite>
+        <TestSuite name="headers">
+          <TestCase itShould="has headers">
+            <FastImage
+              style={styles.image}
+              source={{
+                uri: "http://fpoimg.com/300x300",
+                headers: {
+                  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+                  'Accept-Language': 'en-US,en;q=0.9',
+                  'Connection': 'close'
+                }
+              }}
+            />
+          </TestCase>
+          <TestCase itShould="no headers">
+            <FastImage
+              style={styles.image}
+              source={{
+                uri: "http://fpoimg.com/300x300",
               }}
             />
           </TestCase>
