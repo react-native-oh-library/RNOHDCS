a7fff959a127608c92366b056c472410a860411f
zy035017 <49781846+zy035017@users.noreply.github.com>
2024-08-21 09:43:54 +0800

    test: [#818] 新增NativeBase测试demo (#882)
    Co-authored-by: duanshao1@h-partners.com <duanshao1@h-partners.com>

---
+++ NativeBase/nativeBaseUiTest/AlertDialog.tsx
@@ -0,0 +1,83 @@
+import {useState, useRef} from 'react';
+import {View, StyleSheet, ScrollView, Text} from 'react-native';
+import {Center, Button, AlertDialog} from 'native-base';
+import {Tester, TestSuite, TestCase} from '@rnoh/testerino';
+
+export function AlertDialogTest() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClose = () => setIsOpen(false);
+
+  const cancelRef = useRef(null);
+  return (
+    <>
+      <Tester>
+        <ScrollView style={styles.content}>
+          <TestSuite name="Basic">
+            <TestCase itShould="Basic" tags={['dev']}>
+              <View style={styles.section}>
+                <Text>Basic</Text>
+                <View style={styles.subSection}>
+                  <Center>
+                    <Button
+                      colorScheme="danger"
+                      onPress={() => setIsOpen(!isOpen)}>
+                      Delete Customer
+                    </Button>
+                    <AlertDialog
+                      leastDestructiveRef={cancelRef}
+                      isOpen={isOpen}
+                      onClose={onClose}>
+                      <AlertDialog.Content>
+                        <AlertDialog.CloseButton />
+                        <AlertDialog.Header>Delete Customer</AlertDialog.Header>
+                        <AlertDialog.Body>
+                          This will remove all data relating to Alex. This
+                          action cannot be reversed. Deleted data can not be
+                          recovered.
+                        </AlertDialog.Body>
+                        <AlertDialog.Footer>
+                          <Button.Group space={2}>
+                            <Button
+                              variant="unstyled"
+                              colorScheme="coolGray"
+                              onPress={onClose}
+                              ref={cancelRef}>
+                              Cancel
+                            </Button>
+                            <Button colorScheme="danger" onPress={onClose}>
+                              Delete
+                            </Button>
+                          </Button.Group>
+                        </AlertDialog.Footer>
+                      </AlertDialog.Content>
+                    </AlertDialog>
+                  </Center>
+                </View>
+              </View>
+            </TestCase>
+          </TestSuite>
+        </ScrollView>
+      </Tester>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  content: {
+    width: '100%',
+    height: '100%',
+  },
+  section: {
+    backgroundColor: '#f2f2f2',
+  },
+  subSection: {
+    display: 'flex',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    gap: 10,
+    width: '100%',
+    padding: 10,
+  },
+});
