8872b4bfaf3f77be8d20e467cadd54563af44c07
yangzx18 <yangzhixian5@h-partners.com>
2024-08-23 15:15:49 +0800

    test: 补充react-native-popup-menu的测试用例

--- react-native-popup-menu/BasicExample.tsx
+++ react-native-popup-menu/BasicExample.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 import {
   Menu,
   MenuProvider,
@@ -10,9 +10,61 @@ import {
 
 const BasicExample = () => (
   <MenuProvider style={{flexDirection: 'column', padding: 30}}>
-    <Text>Hello world!</Text>
-    <Menu onSelect={value => alert(`Selected number: ${value}`)}>
+    <Text style={css.title}>MenuTrigger</Text>
+    <Menu onSelect={value => alert(`Selected number: ${value}`)} style={css.menu}>
       <MenuTrigger text='Select option' />
+      <MenuOptions>
+        <MenuOption value={1} text='One' />
+        <MenuOption value={2}>
+          <Text style={{color: 'red'}}>Two</Text>
+        </MenuOption>
+        <MenuOption value={3} disabled={true} text='Three' />
+      </MenuOptions>
+    </Menu>
+
+    <Text style={css.title}>MenuTrigger disabled = true</Text>
+    <Menu onSelect={value => alert(`Selected number: ${value}`)} style={css.menu}>
+      <MenuTrigger text='Select option' disabled />
+      <MenuOptions>
+        <MenuOption value={1} text='One' />
+        <MenuOption value={2}>
+          <Text style={{color: 'red'}}>Two</Text>
+        </MenuOption>
+        <MenuOption value={3} disabled={true} text='Three' />
+      </MenuOptions>
+    </Menu>
+
+    <Text style={css.title}>MenuTrigger triggerOnLongPress = true</Text>
+    <Menu onSelect={value => alert(`Selected number: ${value}`)} style={css.menu}>
+      <MenuTrigger text='Select option' triggerOnLongPress />
+      <MenuOptions>
+        <MenuOption value={1} text='One' />
+        <MenuOption value={2}>
+          <Text style={{color: 'red'}}>Two</Text>
+        </MenuOption>
+        <MenuOption value={3} disabled={true} text='Three' />
+      </MenuOptions>
+    </Menu>
+
+    <Text style={css.title}>MenuTrigger onAlternativeAction callback</Text>
+    <Menu onSelect={value => alert(`Selected number: ${value}`)} style={css.menu}>
+      <MenuTrigger text='Select option' onAlternativeAction={() => {
+        console.log(` MenuTrigger onAlternativeAction runing`)
+      }} />
+      <MenuOptions>
+        <MenuOption value={1} text='One' />
+        <MenuOption value={2}>
+          <Text style={{color: 'red'}}>Two</Text>
+        </MenuOption>
+        <MenuOption value={3} disabled={true} text='Three' />
+      </MenuOptions>
+    </Menu>
+
+    <Text style={css.title}>MenuTrigger testID = menuTest</Text>
+    <Menu onSelect={value => alert(`Selected number: ${value}`)} style={css.menu}>
+      <MenuTrigger text='Select option' testID="menuTest" onAlternativeAction={() => {
+        console.log(` MenuTrigger onAlternativeAction runing`)
+      }} />
       <MenuOptions>
         <MenuOption value={1} text='One' />
         <MenuOption value={2}>
@@ -23,5 +75,15 @@ const BasicExample = () => (
     </Menu>
   </MenuProvider>
 );
-
+const css = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    marginBottom: 10
+  },
+  menu: {
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 20
+  }
+})
 export default BasicExample;
