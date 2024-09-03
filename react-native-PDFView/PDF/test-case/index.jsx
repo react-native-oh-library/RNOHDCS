94c6503d0c8b43d3fc68c7b6facc330af2477c42
Panlichen1996 <93173064+Panlichen1996@users.noreply.github.com>
2024-09-03 15:31:59 +0800

    test:修改react-native-view-pdf的demo (#1026)

--- react-native-PDFView/PDF/test-case/index.jsx
+++ react-native-PDFView/PDF/test-case/index.jsx
@@ -2,5 +2,6 @@ export * from './PDFFileFromTest';
 export * from './PDFOnErrorTest';
 export * from './PDFOnLoadTest';
 export * from './PDFResourceTest';
+export * from './PDFReloadTest';
 export * from './PDFStyleTest';
 export * from './PDFFadeInDurationTest';
