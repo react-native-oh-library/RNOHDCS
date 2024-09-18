b100c38844078a6e7901ee7228e7fba43b9168da
duanzhao <duanshao1@h-partners.com>
2024-09-18 14:17:21 +0800

    test: 新增react-native-linear-gradient-text测试demo

---
+++ react-native-linear-gradient-text/demo/index.tsx
@@ -0,0 +1,43 @@
+import React, {ReactNode} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+// Within your render function
+export function LinearGradientDemo() {
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={['#4c669f', '#3b5998', '#192f6a']}
+        style={styles.linearGradient}>
+        <Text style={styles.buttonText}>Sign in with Facebook</Text>
+      </LinearGradient>
+      ;
+    </View>
+  );
+}
+
+// Later on in your styles..
+var styles = StyleSheet.create({
+  container: {},
+  linearGradient: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  },
+});
