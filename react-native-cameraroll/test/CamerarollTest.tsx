d509d7d01d3594cf8cd39bcb69764b24563564cc
小董 <102660404+dw18829031738@users.noreply.github.com>
2024-09-20 16:28:35 +0800

    test: 修改react-native-cameraroll测试用例 (#1171)

--- react-native-cameraroll/test/CamerarollTest.tsx
+++ react-native-cameraroll/test/CamerarollTest.tsx
@@ -96,94 +96,6 @@ const [saveUri, setSaveUri] = useState('');
     );
   };
 
-  const SaveToCamerarollInternetImage = (props: {
-    setState: React.Dispatch<React.SetStateAction<string>>;
-  }) => {
-    return (
-      <View style={{ height: 50 }}>
-        <Button
-          title="SaveToCameraroll"
-          onPress={() => {
-            CameraRoll.saveToCameraRoll("https://res.vmallres.com/uomcdn/CN/cms/202408/5442d69d916d4bcf9ee740d595a164fb.jpg").then((res) => {
-              setSaveUri(JSON.stringify(res));
-              props.setState(JSON.stringify(res));
-            });
-          }}
-        />
-      </View>
-    );
-  };
-
-  const SaveToCamerarollLocalImage = (props: {
-    setState: React.Dispatch<React.SetStateAction<string>>;
-  }) => {
-    return (
-      <View style={{ height: 50 }}>
-        <Button
-          title="SaveToCameraroll"
-          onPress={() => {
-            RNFS.downloadFile({
-              fromUrl:
-                'https://res.vmallres.com/uomcdn/CN/cms/202408/5442d69d916d4bcf9ee740d595a164fb.jpg', // URL to download file from
-              toFile: RNFS.DocumentDirectoryPath + '/5442d69d916d4bcf9ee740d595a164fb.jpg',
-            }).promise.then((res)=>{
-              if(res.statusCode === 200){
-                CameraRoll.saveToCameraRoll(`${RNFS.DocumentDirectoryPath}/5442d69d916d4bcf9ee740d595a164fb.jpg`).then((res) => {
-                  setSaveUri(JSON.stringify(res));
-                  props.setState(JSON.stringify(res));
-                });
-              }
-            })
-          }}
-        />
-      </View>
-    );
-  };
-
-  const SaveToCamerarollInternetVideo = (props: {
-    setState: React.Dispatch<React.SetStateAction<string>>;
-  }) => {
-    return (
-      <View style={{ height: 50 }}>
-        <Button
-          title="SaveToCameraroll"
-          onPress={() => {
-            CameraRoll.saveToCameraRoll("https://vod.vmall.com/asset/d42cd405b6c0093a28a49a094d803e1c/play_video/5f1a9a9494ce32411cf679db98ac777a_2.mp4").then((res) => {
-              setSaveUri(JSON.stringify(res));
-              props.setState(JSON.stringify(res));
-            });
-          }}
-        />
-      </View>
-    );
-  };
-
-  const SaveToCamerarollLocalVideo = (props: {
-    setState: React.Dispatch<React.SetStateAction<string>>;
-  }) => {
-    return (
-      <View style={{ height: 50 }}>
-        <Button
-          title="SaveToCameraroll"
-          onPress={() => {
-            RNFS.downloadFile({
-              fromUrl:
-                'https://vod.vmall.com/asset/d42cd405b6c0093a28a49a094d803e1c/play_video/5f1a9a9494ce32411cf679db98ac777a_2.mp4', // URL to download file from
-              toFile: RNFS.DocumentDirectoryPath + '/5f1a9a9494ce32411cf679db98ac777a_2.mp4',
-            }).promise.then((res)=>{
-              if(res.statusCode === 200){
-                CameraRoll.saveToCameraRoll(`${RNFS.DocumentDirectoryPath}/5f1a9a9494ce32411cf679db98ac777a_2.mp4`).then((res) => {
-                  setSaveUri(JSON.stringify(res));
-                  props.setState(JSON.stringify(res));
-                });
-              }
-            })
-          }}
-        />
-      </View>
-    );
-  };
-
   return (
     <View style={styles.container}>
       <View style={styles.inputArea}>
@@ -219,38 +131,6 @@ const [saveUri, setSaveUri] = useState('');
             itShould="saveAsset接口，保存沙箱视频到相册"
             initialState={''}
             arrange={({ setState }) => <SaveAssetLocalVideo setState={setState} />}
-            assert={({ expect, state }) => {
-              expect(state).to.be.string;
-            }}
-          />
-          <TestCase
-            itShould="saveToCameraRoll接口，保存网络图片到相册"
-            initialState={''}
-            arrange={({ setState }) => <SaveToCamerarollInternetImage setState={setState} />}
-            assert={({ expect, state }) => {
-              expect(state).to.be.string;
-            }}
-          />
-           <TestCase
-            itShould="saveToCameraRoll接口，保存沙箱图片到相册"
-            initialState={''}
-            arrange={({ setState }) => <SaveToCamerarollLocalImage setState={setState} />}
-            assert={({ expect, state }) => {
-              expect(state).to.be.string;
-            }}
-          />
-           <TestCase
-            itShould="saveToCameraRoll接口，保存网络视频到相册"
-            initialState={''}
-            arrange={({ setState }) => <SaveToCamerarollInternetVideo setState={setState} />}
-            assert={({ expect, state }) => {
-              expect(state).to.be.string;
-            }}
-          />
-           <TestCase
-            itShould="saveToCameraRoll接口，保存沙箱视频到相册"
-            initialState={''}
-            arrange={({ setState }) => <SaveToCamerarollLocalVideo setState={setState} />}
             assert={({ expect, state }) => {
               expect(state).to.be.string;
             }}
