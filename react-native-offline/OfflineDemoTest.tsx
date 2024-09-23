0df5eda2a4c0dc2c1eb0f6e6e35b71b1dc658fcb
ZhangYu <n035820@archermind.com>
2024-09-23 19:40:22 +0800

    test:修改react-native-offline测试demo

--- react-native-offline/OfflineDemoTest.tsx
+++ react-native-offline/OfflineDemoTest.tsx
@@ -3,21 +3,22 @@ import { ReduxScreen } from './screens/ReduxScreen';
 import { SagasScreen } from './screens/SagasScreen';
 import { CheckInternetConnectionScreen } from './screens/CheckInternetConnectionScreen';
 import { UseIsConnectedScreen } from './screens/UseIsConnectedScreen';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import { NetworkProvider } from 'react-native-offline';
 
 //react-native-offline demo入口
 export const OfflineDemoTest = () => {
     return (
-        <View>
-
-            <ComponentsScreen></ComponentsScreen>
-            <ReduxScreen></ReduxScreen>
-            <SagasScreen></SagasScreen>
-            <CheckInternetConnectionScreen></CheckInternetConnectionScreen>
-            <NetworkProvider pingServerUrl='www.huawei.com'>
-                <UseIsConnectedScreen></UseIsConnectedScreen>
-            </NetworkProvider>
-        </View>
+        <ScrollView>
+            <View>
+                <ComponentsScreen></ComponentsScreen>
+                <ReduxScreen></ReduxScreen>
+                <SagasScreen></SagasScreen>
+                <CheckInternetConnectionScreen></CheckInternetConnectionScreen>
+                <NetworkProvider pingServerUrl='https://www.huawei.com'>
+                    <UseIsConnectedScreen></UseIsConnectedScreen>
+                </NetworkProvider>
+            </View>
+        </ScrollView>
     );
 };
