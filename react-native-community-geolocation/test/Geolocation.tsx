74006d5399b1579be9a251466cea80e6c78e077d
Wen-pin Chou <xbh0902@gmail.com>
2024-09-30 15:39:30 +0800

    test: 修改react-native-geolocation测试用例 (#1253)

--- react-native-community-geolocation/test/Geolocation.tsx
+++ react-native-community-geolocation/test/Geolocation.tsx
@@ -1,105 +1,131 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-import {View, ScrollView, Text} from 'react-native';
-import {Tester, TestSuite, TestCase} from '@rnoh/testerino';
-import {Button} from 'react-native';
+import { View, ScrollView, Text } from 'react-native';
+import { Tester, TestSuite, TestCase } from '@rnoh/testerino';
+import { Button } from 'react-native';
 import Geo from '@react-native-community/geolocation';
 const Meun = [
   {
     key: 'geo_1',
     itShould: '设置位置信息请求配置',
     label: 'setRNConfiguration',
+    isAssert: false,
     onPress: (setState: (arg0: boolean) => void, setValue: (arg0: string) => void) => {
-      Geo.setRNConfiguration({skipPermissionRequests:true})
-      setValue('setRNConfiguration')
+      try {
+        Geo.setRNConfiguration({ skipPermissionRequests: true })
+        setValue('setRNConfiguration:success');
+      } catch (error) {
+        setValue('setRNConfiguration:fail');
+      }
     },
   },
   {
     key: 'geo_2',
     itShould: '请求获取位置信息权限',
     label: 'requestAuthorization',
+    isAssert: false,
     onPress: (
       setState: (arg0: boolean) => void,
       setValue: (arg0: string) => void,
     ) => {
-        Geo.requestAuthorization(()=>{
-          setValue('requestAuthorization: success')
-          setState(true)
-        },error=>{
-            setState(false)
-            setValue(JSON.stringify(error))
-        })
-     
+      Geo.requestAuthorization(() => {
+        setValue('requestAuthorization: success')
+      }, error => {
+        setValue(JSON.stringify(error))
+      })
+
     },
   },
   {
     key: 'geo_3',
     itShould: '获取当前位置信息',
     label: 'getCurrentPosition',
-    onPress: (setState: (arg0: boolean) => void,setValue: (arg0: string) => void,) => {
-            Geo.getCurrentPosition((success)=>{
-                setState(true)
-                setValue(`getCurrentPosition: ${JSON.stringify(success)}`)
-            },error=>{
-                setState(false)
-                setValue(JSON.stringify(error))
-            })
+    isAssert: true,
+    onPress: (setState: (arg0: boolean) => void, setValue: (arg0: string) => void,) => {
+      Geo.getCurrentPosition((success) => {
+        setState(!!success)
+        setValue(`getCurrentPosition: ${JSON.stringify(success)}`)
+      }, error => {
+        setState(!!error)
+        setValue(JSON.stringify(error))
+      })
     },
   },
   {
     key: 'geo_4',
     itShould: '设置位置信息变化监听，返回watchId',
     label: 'watchPosition',
-    onPress: (setState: (arg0: boolean) => void,setValue: (arg0: string) => void) => {
-     Geo.watchPosition((success)=>{
-        setState(true)
+    isAssert: true,
+    onPress: (setState: (arg0: boolean) => void, setValue: (arg0: string) => void) => {
+      Geo.watchPosition((success) => {
+        setState(!!success)
         setValue(`watchPosition: ${JSON.stringify(success)}`)
-    },error=>{
-        setState(false)
+      }, error => {
+        setState(!!error)
         setValue(JSON.stringify(error))
-    })
+      })
     },
   },
   {
     key: 'geo_5',
     itShould: '通过watchId清除监听',
+    isAssert: false,
     label: 'clearWatch',
     onPress: (setState: (arg0: boolean) => void, setValue: (arg0: string) => void) => {
-     Geo.clearWatch(0)
-     setValue('clearWatch')
+      Geo.clearWatch(0)
+      setValue('clearWatch')
     },
   },
- 
+
 ];
-export const ProgressViewDemo = () => {
+export const GeoLocationDemo = () => {
   const [value, setValue] = useState('');
   return (
     <Tester>
       <ScrollView>
         <TestSuite name="@react-native-community/geolocation">
           <Text style={{ backgroundColor: '#fff' }}>{value}</Text>
-          {Meun.map((item) => (
-            <TestCase
-              key={item.key}
-              itShould={item.itShould}
-              tags={['C_API']}
-              initialState={false}
-              arrange={({setState}) => {
-                return (
-                  <View style={{flex: 1}}>
-                    <Button
-                      title={item.label}
-                      onPress={() => {
-                        item.onPress(setState, setValue);
-                      }}></Button>
-                  </View>
-                );
-              }}
-              assert={async ({expect, state}) => {
-                expect(state).to.be.true;
-              }}
-            />
-          ))}
+          {Meun.map((item) => {
+            if (item.isAssert) {
+              return (
+                <TestCase
+                  key={item.key}
+                  itShould={item.itShould}
+                  tags={['C_API']}
+                  initialState={false}
+                  arrange={({ setState }) => {
+                    return (
+                      <View style={{ flex: 1 }}>
+                        <Button
+                          title={item.label}
+                          onPress={() => {
+                            item.onPress(setState, setValue);
+                          }}></Button>
+                      </View>
+                    );
+                  }}
+                  assert={async ({ expect, state }) => {
+                    expect(state).to.be.true;
+                  }}
+                />
+              )
+            }
+            return (
+              <TestCase
+                key={item.key}
+                itShould={item.itShould}
+                tags={['C_API']}
+              >
+                <View style={{ flex: 1 }}>
+                  <Button
+                    title={item.label}
+                    onPress={() => {
+                      item.onPress(() => { }, setValue);
+                    }}></Button>
+                </View>
+              </TestCase>
+            )
+          })}
         </TestSuite>
       </ScrollView>
     </Tester>
