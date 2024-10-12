1ddb4cb7aa259f08505f70b21786de876e5fa3b2
haoxuchao <47875207+haoxuchao@users.noreply.github.com>
2024-10-12 16:33:03 +0800

    test:修改react-native-waterflow-list测试demo (#1292)
    Signed-off-by: haoxuchao <haoxuchao@h-partners.com>

--- react-native-waterflow-list/wateflowListDemo.tsx
+++ react-native-waterflow-list/wateflowListDemo.tsx
@@ -1,113 +1,48 @@
-import * as React from 'react';
-import {Dimensions, Image, RefreshControl, Text, View} from 'react-native';
-import {IColumnsHandles} from 'react-native-waterflow-list/src/Columns';
-import WaterFlow from 'react-native-waterflow-list/src/';
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import waterflowList1 from './waterflow_list1';
+import waterflowList2 from './waterflow_list2';
 
-const width = Math.round((Dimensions.get('screen').width - 30) / 2);
+const Stack = createStackNavigator();
 
-const getItemData = (() => {
-    let id = 0;
-    return () => {
-        id++;
-        const height = Math.ceil(Math.random() * 1000);
-        return {
-            id,
-            text: Math.random(),
-            image_path: `https://picsum.photos/${width}/${height}/?random`,
-            height,
-            width,
-        };
-    };
-})();
+class Home extends React.Component {
+  onPress1 = () => {
+      this.props.navigation.navigate('waterflow_list1');
+  }
+  onPress2 = () => {
+      this.props.navigation.navigate('waterflow_list2');
+  }
 
-const sleep = (ms: number) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  render() {
+      return (
+          <View style={{padding:14}}>
+              <View style={{height:50}}>
+                  <Button title="waterflow_list1"  onPress={this.onPress1}></Button>
+              </View>
+              <View style={{height:50,marginTop:12}}>
+                  <Button title="waterflow_list2"  onPress={this.onPress2}></Button>
+              </View>
+              
+          </View>
+      );
+  }
 };
 
-const itemDataFactory = () =>
-    Array(10)
-        .fill('')
-        .map(() => getItemData());
-
-interface IItem {
-    id: number
-
-    [index: string]: any
+class waterflowListDemo extends React.Component {
+    render() {
+        return (
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="waterflowList 组件测试">
+                    <Stack.Screen name="首页" component={Home} />
+                    <Stack.Screen name="waterflow_list1" component={waterflowList1} />
+                    <Stack.Screen name="waterflow_list2" component={waterflowList2} />
+                  
+                </Stack.Navigator>
+            </NavigationContainer>
+        );
+    }
 }
 
-export default () => {
-    const [data, setData] = React.useState<IItem[]>([]);
-    const [loading, setLoading] = React.useState(false)
-
-    const WaterFlowRef = React.useRef<IColumnsHandles>()
-    const onLoadMore = React.useCallback(async () => {
-        setLoading(true)
-        await sleep(1000);
-        setLoading(false)
-        return setData(data.concat(itemDataFactory()));
-    }, [data]);
-    const loadData = React.useCallback(async () => {
-        await sleep(1000);
-        return setData(itemDataFactory());
-    }, [data])
-
-    React.useEffect(() => {
-        setData(itemDataFactory());
-    }, []);
-
-    return (
-        <WaterFlow
-            ref={WaterFlowRef}
-            data={data}
-            keyForItem={item => item.id}
-            numColumns={2}
-            onEndReached={onLoadMore}
-            /** 允许 heightForItem 为异步函数 */
-            // asyncHeightForItem={async item => {
-            //   let height = 0
-            //   try {
-            //     height = await (new Promise<number>((resolve, reject) => {
-            //       Image.getSize(item.image_path, (_, imageHeight) => {
-            //         resolve(imageHeight)
-            //       }, reject)
-            //     }))
-            //   } catch (err) { console.log({ err }); }
-            //   return height;
-            // }}
-            /** 如果高度已知则传此方法 */
-            // heightForItem={item => {
-            //   return item.height;
-            // }}
-            columnFlatListProps={{
-                style: {marginHorizontal: 5,},
-            }}
-            columnsFlatListProps={{
-                ListHeaderComponent: () => <View><Text>Hello</Text></View>,
-                refreshControl: <RefreshControl
-                    style={{zIndex: 10}}
-                    refreshing={loading}
-                    onRefresh={() => {
-                        WaterFlowRef.current?.clear()
-                        loadData()
-                    }}
-                    tintColor={'gray'}
-                />
-                ,
-                style: {marginHorizontal: 10,},
-            }}
-            renderItem={({item, index}) => {
-                return renderItem(item);
-            }}
-        />
-    );
-};
-
-const renderItem = item => {
-    return (
-        <View style={{marginHorizontal: 5, paddingTop: 10}}>
-            <Image style={{height: item.height, width: `100%`}} source={{uri: item.image_path}}/>
-            <Text>ID:{item.id}</Text>
-            <Text>{item.text}</Text>
-        </View>
-    );
-};
+export default waterflowListDemo;
