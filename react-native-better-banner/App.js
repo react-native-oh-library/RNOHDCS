3cf76553fd98c6748fae88257c0cf52fdb955d49
duanshao1@h-partners.com <duanshao1@h-partners.com>
2024-10-17 15:37:07 +0800

    test: [#1321] 修改react-native-better-banner测试Demo

--- react-native-better-banner/App.js
+++ react-native-better-banner/App.js
@@ -1,55 +1,55 @@
-import {Tester, TestSuite} from '@rnoh/testerino';
-import React, {useState} from 'react';
-import {TestCase} from './testCase';
+import { Tester, TestSuite } from '@rnoh/testerino';
+import React, { useState } from 'react';
+import { TestCase } from './testCase';
 import BetterBanner from 'react-native-better-banner';
 import { Text, View, StyleSheet, ScrollView } from 'react-native';
 
 const bannerComponents = [
   <View style={{
-      width: '100%',
-      height: '100%',
-      backgroundColor: '#1997fc',
-      alignItems: 'center',
-      justifyContent: 'center'
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#1997fc',
+    alignItems: 'center',
+    justifyContent: 'center'
   }}>
-      <Text style={{fontSize: 35, color: '#fff', marginBottom: 10}}>Page 01</Text>
-      <Text style={{fontSize: 15, color: '#fff'}}>Welcome! have a good time</Text>
+    <Text style={{ fontSize: 35, color: '#fff', marginBottom: 10 }}>Page 01</Text>
+    <Text style={{ fontSize: 15, color: '#fff' }}>Welcome! have a good time</Text>
   </View>,
   <View style={{
-      width: '100%',
-      height: '100%',
-      backgroundColor: '#da578f',
-      alignItems: 'center',
-      justifyContent: 'center'
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#da578f',
+    alignItems: 'center',
+    justifyContent: 'center'
   }}>
-      <Text style={{fontSize: 35, color: '#fff', marginBottom: 10}}>Page 02</Text>
-      <Text style={{fontSize: 15, color: '#fff'}}>Welcome! have a good time</Text>
+    <Text style={{ fontSize: 35, color: '#fff', marginBottom: 10 }}>Page 02</Text>
+    <Text style={{ fontSize: 15, color: '#fff' }}>Welcome! have a good time</Text>
   </View>,
   <View style={{
-      width: '100%',
-      height: '100%',
-      backgroundColor: '#7c3fe4',
-      alignItems: 'center',
-      justifyContent: 'center'
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#7c3fe4',
+    alignItems: 'center',
+    justifyContent: 'center'
   }}>
-      <Text style={{fontSize: 35, color: '#fff', marginBottom: 10}}>Page 03</Text>
-      <Text style={{fontSize: 15, color: '#fff'}}>Welcome! have a good time</Text>
+    <Text style={{ fontSize: 35, color: '#fff', marginBottom: 10 }}>Page 03</Text>
+    <Text style={{ fontSize: 15, color: '#fff' }}>Welcome! have a good time</Text>
   </View>,
   <View style={{
-      width: '100%',
-      height: '100%',
-      backgroundColor: '#6d42f7',
-      alignItems: 'center',
-      justifyContent: 'center'
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#6d42f7',
+    alignItems: 'center',
+    justifyContent: 'center'
   }}>
-      <Text style={{fontSize: 35, color: '#fff', marginBottom: 10}}>Page 04</Text>
-      <Text style={{fontSize: 15, color: '#fff'}}>Welcome! have a good time</Text>
+    <Text style={{ fontSize: 35, color: '#fff', marginBottom: 10 }}>Page 04</Text>
+    <Text style={{ fontSize: 15, color: '#fff' }}>Welcome! have a good time</Text>
   </View>
 ]
 const bannerImages = [
-  {uri: "http://attachments.gfan.net.cn/forum/attachments2/201402/05/211339r5eizavo2g5efuar.jpg.thumb.jpg"},
-  {uri: "http://attachments.gfan.net.cn/forum/attachments2/201402/05/211342pkd8axiibj5axizi.jpg.thumb.jpg"},
-  {uri: "http://attachments.gfan.net.cn/forum/attachments2/201402/05/2113441e4dw5zvv49jkdow.jpg.thumb.jpg"},
+  { uri: "https://developer.huawei.com/images/new-content/develop/img_DLP_develop_gailan_ArkUI.png" },
+  { uri: "https://developer.huawei.com/images/new-content/develop/img_DLP_develop_gailan_ArkUI.png" },
+  { uri: "https://developer.huawei.com/images/new-content/develop/img_DLP_develop_gailan_ArkUI.png" },
 ]
 
 const BetterBannerTest = () => {
@@ -57,6 +57,29 @@ const BetterBannerTest = () => {
     <ScrollView>
       <Tester>
         <TestSuite name="BetterBanner" style={styles.container}>
+
+          <TestCase.Example
+            skip={{ android: true, harmony: { arkTS: true, cAPI: true } }}
+            itShould="adaptSeamlessScrollValue">
+            <BetterBannerTestCase8 />
+          </TestCase.Example>
+
+          <TestCase.Example
+            skip={{ android: true, harmony: { arkTS: true, cAPI: true } }}
+            itShould="isSeamlessScroll-值fales翻到最后一页再往后翻不能回到第一页">
+            <BetterBannerTestCase7 />
+          </TestCase.Example>
+
+          <TestCase.Example
+            skip={{ android: true, harmony: { arkTS: true, cAPI: true } }}
+            itShould="bannerTitleTextColor-值red">
+            <BetterBannerTestCase6 />
+          </TestCase.Example>
+          <TestCase.Example
+            skip={{ android: true, harmony: { arkTS: true, cAPI: true } }}
+            itShould="indicatorGroupPosition-值left">
+            <BetterBannerTestCase0 />
+          </TestCase.Example>
           <TestCase.Example
             skip={{ android: true, harmony: { arkTS: true, cAPI: true } }}
             itShould="display red background if Screen Reader is enabled">
@@ -87,20 +110,110 @@ const BetterBannerTest = () => {
     </ScrollView>
   );
 };
-const BetterBannerTestCase1 = () => {
+
+const BetterBannerTestCase8 = () => {
   return <View>
     <BetterBanner
       bannerComponents={bannerComponents}
-      bannerHeight={ 250 }
+      bannerHeight={250}
       bannerTitles={["Page 01 1111", "Page 02 2222", "Page 03 3333", "Page 04 4444"]}
-      bannerTitleTextColor='#fff'
-      bannerTitleTextSize={ 14 }
-      scrollInterval={ 2000 }
+      bannerTitleTextColor='red'
+      bannerTitleTextSize={14}
+      scrollInterval={2000}
       indicatorWidth={20}
       indicatorHeight={12}
       indicatorColor={'rgba(255,255,255,0.6)'}
       activeIndicatorColor={'#fff'}
-      onScrollEnd={()=>{console.log('onScrollEnd')}}
+      onScrollEnd={() => { console.log('onScrollEnd') }}
+      indicatorContainerBackgroundColor={'rgba(0,0,0,0.3)'}
+      onPress={(index) => alert('you pressed index is : ' + index)}
+      indicatorGroupPosition={'left'}
+      isSeamlessScroll={true}
+      adaptSeamlessScrollValue={true}
+    />
+  </View>;
+};
+
+const BetterBannerTestCase7 = () => {
+  return <View>
+    <BetterBanner
+      bannerComponents={bannerComponents}
+      bannerHeight={250}
+      bannerTitles={["Page 01 1111", "Page 02 2222", "Page 03 3333", "Page 04 4444"]}
+      bannerTitleTextColor='red'
+      bannerTitleTextSize={14}
+      scrollInterval={2000}
+      indicatorWidth={20}
+      indicatorHeight={12}
+      indicatorColor={'rgba(255,255,255,0.6)'}
+      activeIndicatorColor={'#fff'}
+      onScrollEnd={() => { console.log('onScrollEnd') }}
+      indicatorContainerBackgroundColor={'rgba(0,0,0,0.3)'}
+      onPress={(index) => alert('you pressed index is : ' + index)}
+      indicatorGroupPosition={'left'}
+      isSeamlessScroll={false}
+    />
+  </View>;
+};
+
+
+const BetterBannerTestCase6 = () => {
+  return <View>
+    <BetterBanner
+      bannerComponents={bannerComponents}
+      bannerHeight={250}
+      bannerTitles={["Page 01 1111", "Page 02 2222", "Page 03 3333", "Page 04 4444"]}
+      bannerTitleTextColor='red'
+      bannerTitleTextSize={14}
+      scrollInterval={2000}
+      indicatorWidth={20}
+      indicatorHeight={12}
+      indicatorColor={'rgba(255,255,255,0.6)'}
+      activeIndicatorColor={'#fff'}
+      onScrollEnd={() => { console.log('onScrollEnd') }}
+      indicatorContainerBackgroundColor={'rgba(0,0,0,0.3)'}
+      onPress={(index) => alert('you pressed index is : ' + index)}
+      indicatorGroupPosition={'left'}
+      isSeamlessScroll={true}
+    />
+  </View>;
+};
+
+const BetterBannerTestCase0 = () => {
+  return <View>
+    <BetterBanner
+      bannerComponents={bannerComponents}
+      bannerHeight={250}
+      bannerTitleTextColor='#fff'
+      bannerTitleTextSize={14}
+      scrollInterval={2000}
+      indicatorWidth={20}
+      indicatorHeight={12}
+      indicatorColor={'rgba(255,255,255,0.6)'}
+      activeIndicatorColor={'#fff'}
+      onScrollEnd={() => { console.log('onScrollEnd') }}
+      indicatorContainerBackgroundColor={'rgba(0,0,0,0.3)'}
+      onPress={(index) => alert('you pressed index is : ' + index)}
+      indicatorGroupPosition={'left'}
+      isSeamlessScroll={true}
+    />
+  </View>;
+};
+
+const BetterBannerTestCase1 = () => {
+  return <View>
+    <BetterBanner
+      bannerComponents={bannerComponents}
+      bannerHeight={250}
+      bannerTitles={["Page 01 1111", "Page 02 2222", "Page 03 3333", "Page 04 4444"]}
+      bannerTitleTextColor='#fff'
+      bannerTitleTextSize={14}
+      scrollInterval={2000}
+      indicatorWidth={20}
+      indicatorHeight={12}
+      indicatorColor={'rgba(255,255,255,0.6)'}
+      activeIndicatorColor={'#fff'}
+      onScrollEnd={() => { console.log('onScrollEnd') }}
       indicatorContainerBackgroundColor={'rgba(0,0,0,0.3)'}
       onPress={(index) => alert('you pressed index is : ' + index)}
       isSeamlessScroll={true}
@@ -112,15 +225,15 @@ const BetterBannerTestCase2 = () => {
   return <View style={styles.container}>
     <BetterBanner
       bannerComponents={bannerComponents}
-      bannerHeight={ 500 }
+      bannerHeight={500}
       bannerTitles={["Page 01 1111", "Page 02 2222", "Page 03 3333", "Page 04 4444"]}
-      bannerTitleTextColor='#fff'
-      bannerTitleTextSize={ 14 }
-      scrollInterval={ 2000 }
-      isAutoScroll={ false }
+      bannerTitleTextColor='pink'
+      bannerTitleTextSize={14}
+      scrollInterval={2000}
+      isAutoScroll={false}
       indicatorWidth={20}
       indicatorHeight={12}
-      onScrollEnd={()=>{console.log('onScrollEnd')}}
+      onScrollEnd={() => { console.log('onScrollEnd') }}
       onPress={(index) => alert('you pressed index is : ' + index)}
       isSeamlessScroll={true}
       indicatorGroupPosition={'center'}
@@ -130,19 +243,19 @@ const BetterBannerTestCase2 = () => {
 
 const BetterBannerTestCase3 = () => {
   const [count, setCount] = useState(100)
-  const onPress = (index)=> {
+  const onPress = (index) => {
     setCount(1000)
-    alert(count+index)
+    alert(count + index)
   }
   return <View style={styles.container}>
     <BetterBanner
       bannerComponents={bannerComponents}
-      bannerHeight={ 250 }
+      bannerHeight={250}
       bannerTitles={["Page 01 Page 01 Page 01 Page 01 Page 01 Page 01 Page 01 ", "Page 02 6666", "Page 03 8888", "Page 04 6688"]}
       bannerTitleTextColor='#000'
-      bannerTitleTextSize={ 14 }
-      scrollInterval={ 3000 }
-      isAutoScroll={ false }
+      bannerTitleTextSize={14}
+      scrollInterval={3000}
+      isAutoScroll={false}
       indicatorWidth={20}
       indicatorHeight={12}
       indicatorColor={'#000'}
@@ -161,14 +274,14 @@ const BetterBannerTestCase4 = () => {
     <BetterBanner
       bannerComponents={bannerComponents}
       bannerTitles={["Page 01 Page 01 Page 01 Page 01 Page 01 Page 01 Page 01 ", "Page 02 6666", "Page 03 8888", "Page 04 6688"]}
-      scrollInterval={ 4000 }
-      isAutoScroll={ true }
+      scrollInterval={4000}
+      isAutoScroll={true}
       indicatorColor={'#000'}
       indicatorGap={24}
       activeIndicatorColor={'#000'}
       indicatorGroupSideOffset={30}
       indicatorContainerHeight={64}
-      onScrollEnd={()=>{console.log('onScrollEnd')}}
+      onScrollEnd={() => { console.log('onScrollEnd') }}
       indicatorContainerBackgroundColor={'rgba(0,0,0,0.3)'}
       onPress={(index) => alert('you pressed index is : ' + index)}
       isSeamlessScroll={true}
@@ -183,7 +296,7 @@ const BetterBannerTestCase5 = () => {
       bannerImages={bannerImages}
       bannerTitles={["Page 01 Page 01 Page 01 Page 01 Page 01 Page 01 Page 01 ", "Page 02 6666", "Page 03 8888", "Page 04 6688"]}
       bannerTitleTextColor='#000'
-      isAutoScroll={ false }
+      isAutoScroll={false}
       indicatorWidth={20}
       indicatorHeight={12}
       indicatorColor={'#000'}
@@ -191,7 +304,7 @@ const BetterBannerTestCase5 = () => {
       activeIndicatorColor={'#000'}
       indicatorGroupSideOffset={40}
       indicatorContainerHeight={72}
-      onScrollEnd={()=>{console.log('onScrollEnd')}}
+      onScrollEnd={() => { console.log('onScrollEnd') }}
       indicatorContainerBackgroundColor={'rgba(0,0,0,0.3)'}
       onPress={(index) => alert('you pressed index is : ' + index)}
       isSeamlessScroll={true}
@@ -202,7 +315,7 @@ const BetterBannerTestCase5 = () => {
 
 const styles = StyleSheet.create({
   container: {
-      flex: 1
+    flex: 1
   }
 });
 
