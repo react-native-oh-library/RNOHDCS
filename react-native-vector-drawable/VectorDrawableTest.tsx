84536abaf35e6ce1a86b8d9aa35a2f19f5204367
huangzz1106 <106304144+huangzz1106@users.noreply.github.com>
2024-10-17 17:13:37 +0800

    test:修改react-native-vetor-drawable demo (#1323)

--- react-native-vector-drawable/VectorDrawableTest.tsx
+++ react-native-vector-drawable/VectorDrawableTest.tsx
@@ -6,6 +6,7 @@ import {
     TextInput,
     ScrollView,
     StyleSheet,
+    Image,
     Button
 } from 'react-native';
 import {
@@ -15,10 +16,11 @@ import {
 } from '@rnoh/testerino';
 import VectorDrawable from "react-native-vector-drawable";
 export function VectorDrawableTest(): JSX.Element {
+    const source = require('../assets/react-native-community-hooks.jpg')
     return (
         <Tester style={{ flex: 1 }}>
             <TestSuite name="vectordrawable">
-                <TestCase itShould="drawable" tags={['Se_Ra']}
+                <TestCase itShould="drawable base" tags={['Se_Ra']}
                 >
                     <ScrollView>
                         <Text>对标源库IOS开发，此处空白属于正常</Text>
@@ -27,6 +29,27 @@ export function VectorDrawableTest(): JSX.Element {
                             style={{ width: 50, height: 50, tintColor: 'blue' }}
                         />
                     </ScrollView>
+                </TestCase>
+                <TestCase itShould="drawable 文本" tags={['Se_Ra']}
+                >
+                    <VectorDrawable
+                        resourceName="ic_drawable_name"
+                        style={{ width: 80, height: 80, backgroundColor: 'red' }}
+                    >
+                        <Text>width:80 height:80</Text>
+                        <Text>背景：red</Text>
+                    </VectorDrawable>
+                </TestCase>
+                <TestCase itShould="drawable 嵌套图片" tags={['Se_Ra']}
+                >
+                    <VectorDrawable
+                        resourceName="ic_drawable_name"
+                    >
+                        <Image
+                            style={{ width: 100, height: 100, margin: 4, borderRadius: 8 }}
+                            source={source}
+                        />
+                    </VectorDrawable>
                 </TestCase>
             </TestSuite>
         </Tester>
