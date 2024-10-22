66158ef57a3bdff29147b57ba2904507926ffd72
r d <513149011@qq.com>
2024-10-22 01:25:00 +0000

    增加appid输入框

--- react-native-alipay/test/ReactNativeAlipayTest.tsx
+++ react-native-alipay/test/ReactNativeAlipayTest.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Button } from 'react-native';
 import { Tester, TestSuite, TestCase } from '@rnoh/testerino';
 import Alipay from "react-native-alipay";
 import { AFAuthServiceResponse } from '@alipay/afservicesdk';
@@ -8,6 +8,7 @@ import { AFAuthServiceResponse } from '@alipay/afservicesdk';
 export default function App(): JSX.Element {
     const [bytesString, setBytesString] = useState('');
     const [size, setSize] = useState('30');
+    const [value, onChangeText] = React.useState('请输入订单号');
 
     return (
         <Tester style = {{flex: 1, top: 30}}>
@@ -21,19 +22,28 @@ export default function App(): JSX.Element {
                     initialState = {undefined as any}
                     arrange = {({setState}) => {
                         return (
-                            <TouchableOpacity 
-                                onPress = {async () => {
-                                    let orderInfo = 'app_id=2014100900013222&biz_content=%7B%22timeout_express%22%3A%2230m%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22total_amount%22%3A%220.01%22%2C%22subject%22%3A%221%22%2C%22body%22%3A%22xxx%22%2C%22out_trade_no%22%3A%22723175011179269%22%7D&charset=utf-8&method=alipay.trade.app.pay&sign_type=RSA2&timestamp=2016-07-29%2016%3A55%3A53&version=1.0&sign=ClyziyfaiJtGd01HE1XmLbr%2BGy1JzBiH7QF69Qb66GvB%2BH6ULI5BeqEHeJIxVtKKgM%2F0ILEz7XjH2bvP1Fj52VUg5Gc5sg2reVHr8EhHqY7IiSAkEt3lNckWhfeVvdhjbQEKKZWMPgVC%2FVmMvMkkFzsP5S5Zz3aZKZWRp7xLApWtBZTeJj2Hd%2FDNhbEzvpTiQF9UDs1hPMmXmKLrNUmi0k4MQaeBCxXxmY9ITTmHofsju30rxo3q%2FYMYWD79ayxFmSO6adAXP9nLY%2B16VyeBCb53zxb%2BPWjV4CCVmit8YBOIOssAN4B0yLlmBOYWJS76MM46ADu%2FmiCwkdOnJduC6Q%3D%3D';
-                                    console.info('App getOrderInfoStr: ' + orderInfo);
-                                    Alipay.alipay(orderInfo).then((result: any) => {
-                                        console.info('App alipay result: ' + result);
-                                        setState(result);
-                                    });
-                                }}
-                                style = {styles.btn}>
-                                <Text style = {styles.btnText} > 支付宝支付 </Text>
-                            </TouchableOpacity>
-                        );
+                            <>
+                            <TextInput
+                                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                                onChangeText={text => onChangeText(text)}
+                                value={value}
+                            />
+                            <Button
+                                title="submit"
+                                onPress={
+                                    async () => {
+                                        let orderInfo = value;
+                                        console.info('App getOrderInfoStr: ' + orderInfo);
+                                        Alipay.alipay(orderInfo).then((result: any) => {
+                                            console.info('App alipay result: ' + result);
+                                            setState(result);
+                                        });
+                                    }
+                                }
+                            />
+                            </>
+                        )
+         
                     }}
                     assert = {async ({expect, state}) => {
                         expect(state).to.have.string("9000"); // 9000 业务调用成功
