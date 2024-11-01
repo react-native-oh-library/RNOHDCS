ab68d63e3d1c7af383156f587747c3f50f591e4d
xuxiaoqian1 <164186826+xuxiaoqian1@users.noreply.github.com>
2024-11-01 10:04:41 +0800

    test: 修改react-native-gifted-charts测试用例 (#1480)
    Co-authored-by: xuxiaoqain01 <xuwenqian8@h-partners.com>

--- react-native-gifted-charts/tester/LineChartPointerConfigProps.jsx
+++ react-native-gifted-charts/tester/LineChartPointerConfigProps.jsx
@@ -9,21 +9,22 @@ import { axesProps, commonPointerProps } from "../commonProps";
 export default function () {
   const scrollref = useRef();
   const [lineData, setLineData] = useState([{ value: 50, label: '50' }, { value: 80, label: '80' }, { value: 90, label: '90' }, { value: 70, label: '70' }]);
+  
+  const data1 = [  {value: 10}, {value: 20}, {value: 40}, {value: 60}, ];
+  const data2 = [  {value: 50}, {value: 80}, {value: 90}, {value: 70}, ];
   const pointerConfigProps = [
     ...commonPointerProps,
     { pointerConfig: { pointer1Color: 'green' }, }, { pointerConfig: { pointer1Color: 'gray' }, },
-    { pointerConfig: { secondaryPointerColor: 'green' }, }, { pointerConfig: { secondaryPointerColor: 'gray' }, },
-    { pointerConfig: { hideSecondaryPointer: true }, }, { pointerConfig: { hideSecondaryPointer: false }, },
-
     {
       pointerConfig: {
+        pointer1Color: 'yellow',
         pointer2Color: 'green',
-        pointer2Color: 'yellow',
         pointer3Color: 'blue',
         pointer4Color: 'gray',
         pointer5Color: 'black'
       },
       data2: [{ value: 70 }, { value: 85 }, { value: 95 }, { value: 100 }],
+      data1: [{ value: 50 }, { value: 80 }, { value: 90 }, { value: 70 }],
       data3: [{ value: 40 }, { value: 55 }, { value: 65 }, { value: 75 }],
       data4: [{ value: 25 }, { value: 35 }, { value: 45 }, { value: 55 }],
       data5: [{ value: 10 }, { value: 20 }, { value: 30 }, { value: 40 }],
@@ -64,16 +65,17 @@ export default function () {
       data4: [{ value: 25 }, { value: 35 }, { value: 45 }, { value: 55 }],
       data5: [{ value: 10 }, { value: 20 }, { value: 30 }, { value: 40 }],
     },
-    {
-      pointerConfig: {
-        pointerColorsForDataSet: ['green', 'yellow', 'blue', 'blue', 'black']
-      },
-      data2: [{ value: 70 }, { value: 85 }, { value: 95 }, { value: 100 }],
-      data3: [{ value: 40 }, { value: 55 }, { value: 65 }, { value: 75 }],
-      data4: [{ value: 25 }, { value: 35 }, { value: 45 }, { value: 55 }],
-      data5: [{ value: 10 }, { value: 20 }, { value: 30 }, { value: 40 }],
-    },
   ]
+  const aa ={      
+    pointerConfig: {
+      pointerColorsForDataSet: ['green', 'yellow', 'blue', 'blue', 'black'],
+    },
+    dataSet:[
+        {data : [{ value: 50 }, { value: 80 }, { value: 90 }, { value: 70 },] },
+        {data : [{ value: 10 }, { value: 20 }, { value: 30 }, { value: 40 }] }
+    ],
+}
+
   return (
     <Tester>
       <ScrollView>
@@ -87,6 +89,34 @@ export default function () {
             )
           })
         }
+        <TestCase itShould={'s'} tags={['C_API']}>
+          <Text>请点击第一个顶点上显示的数据点，观察效果</Text>
+          <LineChart pointerConfig= { {pointerColorsForDataSet: ['green', 'yellow', 'blue', 'blue', 'black']}}
+            dataSet = {[
+              {data : [{ value: 50 }, { value: 80 }, { value: 90 }, { value: 70 },] },
+              {data : [{ value: 10 }, { value: 20 }, { value: 30 }, { value: 40 }] }
+            ]}
+          ></LineChart>
+        </TestCase>
+        
+
+        <TestCase itShould={`data={${JSON.stringify(data1)}} secondaryData={${JSON.stringify(data2)}} pointerConfig= { {secondaryPointerColor: 'gray'} }`} tags={['C_API']}>
+          <Text>请点击图形上显示的数据点，观察效果</Text>
+          <LineChart scrollRef={scrollref} data={data1} secondaryData={data2} pointerConfig= { {secondaryPointerColor: 'gray'} } ></LineChart>
+        </TestCase>
+        <TestCase itShould={`data={${JSON.stringify(data1)}} secondaryData={${JSON.stringify(data2)}} pointerConfig= { {secondaryPointerColor: 'green'} }`} tags={['C_API']}>
+          <Text>请点击图形上显示的数据点，观察效果</Text>
+          <LineChart scrollRef={scrollref} data={data1} secondaryData={data2} pointerConfig= { {secondaryPointerColor: 'green'} } ></LineChart>
+        </TestCase>
+        <TestCase itShould={`data={${JSON.stringify(data1)}} secondaryData={${JSON.stringify(data2)}} pointerConfig= { {hideSecondaryPointer: true} }`} tags={['C_API']}>
+          <Text>请点击图形上显示的数据点，观察效果</Text>
+          <LineChart scrollRef={scrollref} data={data1} secondaryData={data2} pointerConfig= { {hideSecondaryPointer: true} } ></LineChart>
+        </TestCase>
+        <TestCase itShould={`data={${JSON.stringify(data1)}} secondaryData={${JSON.stringify(data2)}} pointerConfig= { {hideSecondaryPointer: false} }`} tags={['C_API']}>
+          <Text>请点击图形上显示的数据点，观察效果</Text>
+          <LineChart scrollRef={scrollref} data={data1} secondaryData={data2} pointerConfig= { {hideSecondaryPointer: false} } ></LineChart>
+        </TestCase>
+
         <TestCase itShould="{
               pointerConfig: {
                 pointerComponent: (items) => {
