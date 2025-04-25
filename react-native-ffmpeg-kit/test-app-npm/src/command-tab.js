48548655a7e744576a9c3179c3300b6d115c1032
jiang_kui <jiangkui6@h-partners.com>
2025-04-25 14:29:35 +0800

    test:新增react-native-ffmpeg-kit三方库demo
    Signed-off-by: jiang_kui <jiangkui6@h-partners.com>

---
+++ react-native-ffmpeg-kit/test-app-npm/src/command-tab.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import {ScrollView, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {styles} from './style';
+import {ffprint, listFFprobeSessions, notNull} from './util';
+import {
+    FFmpegKit,
+    FFmpegKitConfig,
+    FFprobeSession,
+    Level,
+    LogRedirectionStrategy,
+    SessionState
+} from "ffmpeg-kit-react-native";
+
+export default class CommandTab extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            commandText: '', outputText: ''
+        };
+    }
+
+    componentDidMount() {
+        this.props.navigation.addListener('focus', (_) => {
+           this.clearOutput();
+           this.setActive();
+        });
+    }
+
+    setActive() {
+        ffprint("Command Tab Activated");
+        FFmpegKitConfig.enableLogCallback(undefined);
+        FFmpegKitConfig.enableStatisticsCallback(undefined);
+    }
+
+    appendOutput(logMessage) {
+        this.setState({outputText: this.state.outputText + logMessage});
+    };
+
+    clearOutput() {
+        this.setState({outputText: ''});
+    }
+
+    runFFmpeg = () => {
+        this.clearOutput();
+
+        let ffmpegCommand = this.state.commandText;
+
+        ffprint(`Current log level is ${Level.levelToString(FFmpegKitConfig.getLogLevel())}.`);
+
+        ffprint('Testing FFmpeg COMMAND asynchronously.');
+
+        ffprint(`FFmpeg process started with arguments: \'${ffmpegCommand}\'.`);
+
+        FFmpegKit.execute(ffmpegCommand).then(async (session) => {
+            const state = FFmpegKitConfig.sessionStateToString(await session.getState());
+            const returnCode = await session.getReturnCode();
+            const failStackTrace = await session.getFailStackTrace();
+            const output = await session.getOutput();
+
+            ffprint(`FFmpeg process exited with state ${state} and rc ${returnCode}.${notNull(failStackTrace, "\\n")}`);
+
+            this.appendOutput(output);
+
+            if (state === SessionState.FAILED || !returnCode.isValueSuccess()) {
+                ffprint("Command failed. Please check output for the details.");
+            }
+        });
+    };
+
+    runFFprobe = () => {
+        this.clearOutput();
+
+        let ffprobeCommand = this.state.commandText;
+
+        ffprint('Testing FFprobe COMMAND asynchronously.');
+
+        ffprint(`FFprobe process started with arguments: \'${ffprobeCommand}\'.`);
+
+        FFprobeSession.create(FFmpegKitConfig.parseArguments(ffprobeCommand), async (session) => {
+
+            const state = FFmpegKitConfig.sessionStateToString(await session.getState());
+            const returnCode = await session.getReturnCode();
+            const failStackTrace = await session.getFailStackTrace();
+            session.getOutput().then(output => this.appendOutput(output));
+
+            ffprint(`FFprobe process exited with state ${state} and rc ${returnCode}.${notNull(failStackTrace, "\\n")}`);
+
+            if (state === SessionState.FAILED || !returnCode.isValueSuccess()) {
+                ffprint("Command failed. Please check output for the details.");
+            }
+
+        }, undefined, LogRedirectionStrategy.NEVER_PRINT_LOGS).then(session => {
+            FFmpegKitConfig.asyncFFprobeExecute(session);
+
+            listFFprobeSessions();
+        });
+    };
+
+    render() {
+        return (<View style={styles.screenStyle}>
+            <View style={styles.headerViewStyle}>
+                <Text style={styles.headerTextStyle}>
+                    COMMAND
+                </Text>
+            </View>
+            <View style={styles.textInputViewStyle}>
+                <TextInput
+                    style={styles.textInputStyle}
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    placeholder="Enter command"
+                    underlineColorAndroid="transparent"
+                    onChangeText={(commandText) => this.setState({commandText})}
+                    value={this.state.commandText}
+                />
+            </View>
+            <View style={styles.buttonViewStyle}>
+                <TouchableOpacity
+                    style={styles.buttonStyle}
+                    onPress={this.runFFmpeg}>
+                    <Text style={styles.buttonTextStyle}>RUN FFMPEG</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.buttonViewStyle}>
+                <TouchableOpacity
+                    style={styles.buttonStyle}
+                    onPress={this.runFFprobe}>
+                    <Text style={styles.buttonTextStyle}>RUN FFPROBE</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.outputViewStyle}>
+                <ScrollView
+                    ref={(view) => {
+                        this.scrollViewReference = view;
+                    }}
+                    onContentSizeChange={(width, height) => this.scrollViewReference.scrollTo({y: height})}
+                    style={styles.outputScrollViewStyle}>
+                    <Text style={styles.outputTextStyle}>{this.state.outputText}</Text>
+                </ScrollView>
+            </View>
+        </View>);
+    };
+
+}
---
+++ react-native-ffmpeg-kit/test-app-npm/src/subtitle-tab.js
@@ -0,0 +1,213 @@
+import React from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
+import RNFS from 'react-native-fs';
+import VideoUtil from './video-util';
+import {FFmpegKit, FFmpegKitConfig, ReturnCode} from 'ffmpeg-kit-react-native';
+import {styles} from './style';
+import {ProgressModal} from "./progress_modal";
+import Video from 'react-native-video';
+import {deleteFile, ffprint, notNull} from './util';
+
+export default class SubtitleTab extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            state: 'IDLE',
+            statistics: undefined,
+            sessionId: 0
+        };
+
+        this.progressModalReference = React.createRef();
+    }
+
+    componentDidMount() {
+        this.props.navigation.addListener('focus', (_) => {
+            this.pause();
+            this.setActive();
+        });
+    }
+
+    setActive() {
+        ffprint("Subtitle Tab Activated");
+        FFmpegKitConfig.enableLogCallback(this.logCallback);
+        FFmpegKitConfig.enableStatisticsCallback(this.statisticsCallback);
+    }
+
+    logCallback = (log) => {
+        ffprint(`${log.getSessionId()} -> ${log.getMessage()}`);
+    }
+
+    statisticsCallback = (statistics) => {
+        this.setState({statistics: statistics});
+        this.updateProgressDialog();
+    }
+
+    burnSubtitles = () => {
+        let image1Path = VideoUtil.assetPath(VideoUtil.ASSET_1);
+        let image2Path = VideoUtil.assetPath(VideoUtil.ASSET_2);
+        let image3Path = VideoUtil.assetPath(VideoUtil.ASSET_3);
+        let subtitlePath = VideoUtil.assetPath(VideoUtil.SUBTITLE_ASSET);
+        let videoFile = this.getVideoFile();
+        let videoWithSubtitlesFile = this.getVideoWithSubtitlesFile();
+
+        // IF VIDEO IS PLAYING STOP PLAYBACK
+        this.pause();
+
+        deleteFile(videoFile);
+        deleteFile(videoWithSubtitlesFile);
+
+        ffprint("Testing SUBTITLE burning");
+
+        this.hideProgressDialog();
+        this.showCreateProgressDialog();
+		
+		ffprint(`Testing SUBTITLE burning image1Path: ${image1Path}`);
+		ffprint(`Testing SUBTITLE burning image2Path: ${image2Path}`);
+		ffprint(`Testing SUBTITLE burning image3Path: ${image3Path}`);
+		ffprint(`Testing SUBTITLE burning subtitlePath: ${subtitlePath}`);
+
+        let ffmpegCommand = VideoUtil.generateEncodeVideoScript(image1Path, image2Path, image3Path, videoFile, "mpeg4", "");
+
+        this.setState({state: 'CREATING'});
+
+        FFmpegKit.executeAsync(ffmpegCommand, async (session) => {
+                const state = FFmpegKitConfig.sessionStateToString(await session.getState());
+                const returnCode = await session.getReturnCode();
+                const failStackTrace = await session.getFailStackTrace();
+
+                ffprint(`FFmpeg process exited with state ${state} and rc ${returnCode}.${notNull(failStackTrace, "\\n")}`);
+
+                if (ReturnCode.isSuccess(returnCode)) {
+					this.hideProgressDialog();
+                    let burnSubtitlesCommand = `-y -i ${videoFile} -vf subtitles=${subtitlePath}:force_style='FontName=MyFontName' -c:v mpeg4 ${videoWithSubtitlesFile}`;
+                    //let burnSubtitlesCommand = `-y -i /data/storage/el2/base/haps/entry/cache/video.mp4 -vf subtitles=filename='/data/storage/el2/base/haps/entry/cache/subtitle.srt':force_style='FontName=MyFontName' -c:v mpeg4 /data/storage/el2/base/haps/entry/cache/video-with-subtitles.mp4`;
+					
+                    this.showBurnProgressDialog();
+					
+                    ffprint(`FFmpeg process started with arguments\n\'${burnSubtitlesCommand}\'.`);
+					
+                    this.setState({state: 'BURNING'});
+					
+                    FFmpegKit.executeAsync(burnSubtitlesCommand, async (secondSession) => {
+                        this.hideProgressDialog();
+						
+						ffprint(`Testing SUBTITLE burning secondSessionID: ${secondSession.getSessionId()}`);
+					
+                        const secondState = FFmpegKitConfig.sessionStateToString(await secondSession.getState());
+                        const secondReturnCode = await secondSession.getReturnCode();
+                        const secondFailStackTrace = await secondSession.getFailStackTrace();
+					
+                        if (ReturnCode.isSuccess(secondReturnCode)) {
+                            ffprint("Burn subtitles completed successfully; playing video.");
+                            this.playVideo();
+                        } else if (ReturnCode.isCancel(secondReturnCode)) {
+                            ffprint("Burn subtitles operation cancelled.");
+                            ffprint("Burn subtitles operation cancelled");
+                        } else {
+                            ffprint("Burn subtitles failed. Please check log for the details.");
+                            ffprint(`Burn subtitles failed with state ${secondState} and rc ${secondReturnCode}.${notNull(secondFailStackTrace, "\\n")}`);
+                        }
+                    }).then(session => {
+                        this.setState({sessionId: session.getSessionId()});
+                    });
+                } else {
+                    this.hideProgressDialog();
+                }
+            }
+        ).then(session => {
+            this.setState({sessionId: session.getSessionId()});
+            ffprint(`Async FFmpeg process started with sessionId ${session.getSessionId()}.`)
+        });
+    }
+
+    playVideo() {
+        let player = this.player;
+        if (player !== undefined) {
+            player.seek(0);
+        }
+        this.setState({paused: false});
+    }
+
+    pause() {
+        this.setState({paused: true});
+    }
+
+    getVideoFile() {
+        return `${RNFS.CachesDirectoryPath}/video.mp4`;
+    }
+
+    getVideoWithSubtitlesFile() {
+        return `${RNFS.CachesDirectoryPath}/video-with-subtitles.mp4`;
+    }
+
+    showCreateProgressDialog() {
+        // CLEAN STATISTICS
+        this.setState({statistics: undefined});
+        this.progressModalReference.current.show(`Creating video`, () => FFmpegKit.cancel(this.state.sessionId));
+    }
+
+    showBurnProgressDialog() {
+        // CLEAN STATISTICS
+        this.setState({statistics: undefined});
+        this.progressModalReference.current.show(`Burning subtitles`, () => FFmpegKit.cancel(this.state.sessionId));
+    }
+
+    updateProgressDialog() {
+        let statistics = this.state.statistics;
+        if (statistics === undefined || statistics.getTime() < 0) {
+            return;
+        }
+
+        let timeInMilliseconds = statistics.getTime();
+        let totalVideoDuration = 9000;
+        let completePercentage = Math.round((timeInMilliseconds * 100) / totalVideoDuration);
+
+        if (this.state.state === 'CREATING') {
+            this.progressModalReference.current.update(`Creating video % ${completePercentage}`);
+        } else if (this.state.state === 'BURNING') {
+            this.progressModalReference.current.update(`Burning subtitles % ${completePercentage}`);
+        }
+    }
+
+    hideProgressDialog() {
+        this.progressModalReference.current.hide();
+    }
+
+    onPlayError = (err) => {
+        ffprint('Play error: ' + JSON.stringify(err));
+    }
+
+    render() {
+        return (
+            <View style={styles.screenStyle}>
+                <View style={styles.headerViewStyle}>
+                    <Text
+                        style={styles.headerTextStyle}>
+                        SUBTITLE
+                    </Text>
+                </View>
+                <View style={[styles.buttonViewStyle, {paddingTop: 50, paddingBottom: 50}]}>
+                    <TouchableOpacity
+                        style={[styles.buttonStyle, {width: 160}]}
+                        onPress={this.burnSubtitles}>
+                        <Text style={styles.buttonTextStyle}>BURN SUBTITLES</Text>
+                    </TouchableOpacity>
+                </View>
+                <ProgressModal
+                    visible={false}
+                    ref={this.progressModalReference}/>
+                <Video source={{uri: this.getVideoWithSubtitlesFile()}}
+                       ref={(ref) => {
+                           this.player = ref
+                       }}
+                       hideShutterView={true}
+                       paused={this.state.paused}
+                    // onError={this.onPlayError}
+                       resizeMode={"stretch"}
+                       style={styles.videoPlayerViewStyle}/>
+            </View>
+        );
+    }
+
+}
