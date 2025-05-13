fd090fa7c8ef780b1afe8470a862a216147a6990
tyBrave <103195921+tyBrave@users.noreply.github.com>
2025-05-13 11:08:19 +0800

    chore:修改react-native-reanimated的useAnimatedKeyboards属性使用demo (#1663)
    Signed-off-by: tyBrave <tianyong21@h-partners.com>

--- react-native-reanimated/example/AnimatedKeyboardExample.tsx
+++ react-native-reanimated/example/AnimatedKeyboardExample.tsx
@@ -1,85 +1,45 @@
+import React from 'react';
 import Animated, {
-  KeyboardState,
   useAnimatedKeyboard,
   useAnimatedStyle,
 } from '@react-native-oh-tpl/react-native-reanimated';
-import {
-  Button,
-  Keyboard,
-  ScrollView,
-  StyleSheet,
-  TextInput,
-  View,
-} from 'react-native';
+import { StyleSheet, TextInput, View, useColorScheme } from 'react-native';
 
-import React from 'react';
-
-const BOX_SIZE = 50;
-
-function NestedView() {
-  useAnimatedKeyboard();
-  return <View style={styles.nestedView} />;
-}
 export default function AnimatedKeyboardExample() {
+  const colorScheme = useColorScheme();
   const keyboard = useAnimatedKeyboard();
-  const OPENING = KeyboardState.OPENING;
-  const style = useAnimatedStyle(() => {
-    const color = keyboard.state.value === OPENING ? 'red' : 'blue';
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ translateY: -keyboard.height.value }],
+  }));
 
-    return {
-      backgroundColor: color,
-    };
-  });
-  const translateStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ translateY: -keyboard.height.value }],
-    };
-  });
-  const [shouldShowNestedView, setShouldShowNestedView] = React.useState(false);
   return (
-    <ScrollView
-      contentContainerStyle={styles.container}
-      keyboardDismissMode="interactive"
-      scrollEnabled={false}>
-      <Animated.View style={[styles.box, style]} />
-      <Button
-        title="Toggle nested view"
-        onPress={() => {
-          setShouldShowNestedView(!shouldShowNestedView);
-        }}
-      />
-      {shouldShowNestedView ? <NestedView /> : null}
-      <Animated.View style={translateStyle}>
-        <Button
-          title="Dismiss"
-          onPress={() => {
-            Keyboard.dismiss();
-          }}
-        />
-        <TextInput style={styles.textInput} autoCorrect />
-      </Animated.View>
-    </ScrollView>
+    <Animated.View
+      style={[
+        styles.container,
+        animatedStyles,
+        { backgroundColor: colorScheme === 'light' ? '#fff' : '#000' },
+      ]}>
+      <View style={styles.box}>
+        <TextInput placeholder="Text Input" />
+      </View>
+    </Animated.View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
+    justifyContent: 'center',
+    borderWidth: 5,
+    borderColor: '#782aeb',
+    borderRadius: 2,
+  },
+  box: {
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingVertical: 70,
-  },
-  box: { width: BOX_SIZE, height: BOX_SIZE, marginBottom: 100 },
-  textInput: {
-    borderColor: 'blue',
-    borderStyle: 'solid',
-    borderWidth: 2,
-    height: 60,
-    width: 200,
-  },
-  nestedView: {
-    width: 100,
-    height: 100,
-    backgroundColor: '#ffff00',
+    height: 200,
+    backgroundColor: '#b58df1',
+    borderRadius: 5,
+    margin: 20,
   },
 });
